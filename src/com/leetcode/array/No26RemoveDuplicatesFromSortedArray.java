4a881081882888207e67893860f1dc9140462f0c
Bofeng <bofeng@bitdao.io>
2018-09-28 14:45:33 +0800

    Add 26. Remove Duplicates from Sorted Array.

---
+++ src/com/leetcode/array/No26RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package com.leetcode.array;
+
+public class No26RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+
+        int index = 0;
+        for(int i = 1; i< nums.length;i++){
+            if(nums[i] != nums[index]) nums[++index] = nums[i];
+        }
+
+        return index + 1;
+    }
+}
