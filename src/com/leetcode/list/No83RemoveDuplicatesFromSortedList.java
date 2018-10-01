1a020642c2e9a4cbe1cef8b8bcaf8b1b6130799c
Bofeng Li <bofeng.li@outlook.com>
2018-10-01 21:32:10 +0800

    83. Remove Duplicates from Sorted List

---
+++ src/com/leetcode/list/No83RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,16 @@
+package com.leetcode.list;
+
+public class No83RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
