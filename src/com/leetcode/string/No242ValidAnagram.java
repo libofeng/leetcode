d21115303651e1862010d9139cd17a0e2b3c256e
Bofeng Li <bofeng.li@outlook.com>
2018-10-02 22:19:21 +0800

    242. Valid Anagram

---
+++ src/com/leetcode/string/No242ValidAnagram.java
@@ -0,0 +1,17 @@
+package com.leetcode.string;
+
+public class No242ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < count.length; i++) if (count[i] != 0) return false;
+
+        return true;
+    }
+}
