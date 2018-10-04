b65e2dd717459fbb88a0271ca3f66b1127f983f2
Bofeng Li <bofeng.li@outlook.com>
2018-10-04 12:24:26 +0800

    71. Simplify Path

---
+++ src/com/leetcode/string/No71SimplifyPath.java
@@ -0,0 +1,27 @@
+package com.leetcode.string;
+
+import java.util.Stack;
+
+public class No71SimplifyPath {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (String p : paths) {
+            if (p.length() == 0 || ".".equals(p)) {
+            } else if ("..".equals(p)) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(p);
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        if (stack.isEmpty()) builder.append("/");
+        else {
+            for (String dir : stack) builder.append("/").append(dir);
+        }
+
+        return builder.toString();
+    }
+}
