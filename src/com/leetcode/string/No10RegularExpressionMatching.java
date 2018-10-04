703dbac7eb81cc627bba966e4be3bbb6e51f13bf
Bofeng Li <bofeng.li@outlook.com>
2018-10-04 19:58:10 +0800

    Regular matching.

--- src/com/leetcode/string/No10RegularExpressionMatching.java
+++ src/com/leetcode/string/No10RegularExpressionMatching.java
@@ -1,5 +1,27 @@
 package com.leetcode.string;
 
 public class No10RegularExpressionMatching {
-    //TODO: TBD
+    public boolean isMatch(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
+
+    private boolean matchFirst(String s, int i, String p, int j) {
+        if (i == s.length()) return j == p.length();
+        if (j == p.length()) return i == s.length();
+
+        return s.charAt(i) == p.charAt(j) || '.' == p.charAt(j);
+    }
+
+    private boolean isMatch(String s, int i, String p, int j) {
+        if (j == p.length()) return i == s.length();
+
+        if (j == p.length() - 1 || p.charAt(j + 1) != '*') {
+            if (matchFirst(s, i, p, j)) return isMatch(s, i + 1, p, j + 1);
+            else return false;
+        } else {
+            if (isMatch(s, i, p, j + 2)) return true;
+            while (matchFirst(s, i, p, j)) if (isMatch(s, ++i, p, j + 2)) return true;
+            return false;
+        }
+    }
 }
--- src/com/leetcode/string/No44WildcardMatching.java
+++ src/com/leetcode/string/No44WildcardMatching.java
@@ -1,5 +1,22 @@
 package com.leetcode.string;
 
 public class No44WildcardMatching {
-    // TODO: TBD
+    public boolean isMatch(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
+
+    private boolean isMatch(String s, int i, String p, int j) {
+        if (i == s.length() && j == p.length()) return true;
+        if (i == s.length() && p.charAt(j) == '*') return isMatch(s, i, p, j + 1);
+        if (i == s.length() || j == p.length()) return false;
+
+        if (p.charAt(j) == '*') {
+            while (j < p.length() && p.charAt(j) == '*') j++;
+            if (j == p.length()) return true;
+            while (i < s.length() && !isMatch(s, i, p, j)) i++;
+            return i < s.length();
+        } else if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
+            return isMatch(s, i + 1, p, j + 1);
+        } else return false;
+    }
 }
