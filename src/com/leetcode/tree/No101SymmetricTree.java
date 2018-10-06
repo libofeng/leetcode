768ca16aebacb2ee0571a292b73ec8e2ba8776eb
Bofeng Li <bofeng.li@outlook.com>
2018-10-06 23:15:07 +0800

    101. Symmetric Tree

---
+++ src/com/leetcode/tree/No101SymmetricTree.java
@@ -0,0 +1,47 @@
+package com.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class No101SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        final Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while (!q.isEmpty()) {
+            TreeNode n1 = q.poll(), n2 = q.poll();
+            if (n1 == null && n2 == null) continue;
+            if (n1 == null || n2 == null) return false;
+            if (n1.val != n2.val) return false;
+
+            q.offer(n1.left);
+            q.offer(n2.right);
+            q.offer(n1.right);
+            q.offer(n2.left);
+        }
+
+        return true;
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null) return true;
+        final Stack<TreeNode> s = new Stack<>();
+        s.push(root.left);
+        s.push(root.right);
+        while (!s.isEmpty()) {
+            TreeNode n1 = s.pop(), n2 = s.pop();
+            if (n1 == null && n2 == null) continue;
+            if (n1 == null || n2 == null) return false;
+            if (n1.val != n2.val) return false;
+
+            s.push(n2.right);
+            s.push(n1.left);
+            s.push(n2.left);
+            s.push(n1.right);
+        }
+
+        return true;
+    }
+}
