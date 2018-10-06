9b14f2992004f7367b921cbb57d30c09b6ccb0a8
Bofeng Li <bofeng.li@outlook.com>
2018-10-06 20:34:29 +0800

    107. Binary Tree Level Order Traversal II

--- src/com/leetcode/tree/No107BinaryTreeLevelOrderTraversalII.java
+++ src/com/leetcode/tree/No107BinaryTreeLevelOrderTraversalII.java
@@ -1,4 +1,51 @@
 package com.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class No107BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        final LinkedList<List<Integer>> R = new LinkedList<>();
+        List<TreeNode> level = new LinkedList<>();
+        if (root != null) level.add(root);
+
+        while (!level.isEmpty()) {
+            final List<Integer> r = new LinkedList<>();
+            final List<TreeNode> temp = new LinkedList<>();
+            for (TreeNode node : level) {
+                r.add(node.val);
+                if (node.left != null) temp.add(node.left);
+                if (node.right != null) temp.add(node.right);
+            }
+
+            R.addFirst(r);
+            level = temp;
+        }
+
+        return R;
+    }
+
+
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+        final LinkedList<List<Integer>> R = new LinkedList<>();
+        final Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) q.offer(root);
+
+        while (!q.isEmpty()) {
+            final List<Integer> r = new LinkedList<>();
+            final int len = q.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                r.add(node.val);
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+
+            R.addFirst(r);
+        }
+
+        return R;
+    }
 }
