157a43feacb9102b3a9ed281971c3763ba68033e
Bofeng <bofeng@bitdao.io>
2018-10-23 11:03:50 +0800

    53. Maximum Subarray

---
+++ src/com/leetcode/dp/No53MaximumSubarray.java
@@ -0,0 +1,41 @@
+package com.leetcode.dp;
+
+public class No53MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            maxSum = Math.max(sum, maxSum);
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+
+        return maxSum;
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int maxSum = dp[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+
+        return maxSum;
+    }
+
+    public int maxSubArray3(int[] nums) {
+        int sum = nums[0], maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
