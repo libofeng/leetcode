6e8bb4ff097bff94cd1805696f708a009ea1a581
Bofeng <bofeng@bitdao.io>
2018-10-26 17:15:50 +0800

    120. Triangle

--- src/com/leetcode/dp/No120Triangle.java
+++ src/com/leetcode/dp/No120Triangle.java
@@ -15,6 +15,20 @@ public class No120Triangle {
         return triangle.get(0).get(0);
     }
 
+
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        if (triangle.size() == 0) return 0;
+        final int n = triangle.size();
+        final int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) dp[i] = triangle.get(n - 1).get(i);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+        }
+
+        return dp[0];
+    }
+
     // bottom-up
     public int minimumTotal2(List<List<Integer>> triangle) {
         if (triangle.size() == 0) return 0;
