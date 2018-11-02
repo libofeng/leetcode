a83ef15323f4c2b45fb08b49d923a987a194f6f5
Bofeng Li <bofeng.li@outlook.com>
2018-11-02 08:40:59 +0800

    Update 139. Word Break

--- src/com/leetcode/dp/No139WordBreak.java
+++ src/com/leetcode/dp/No139WordBreak.java
@@ -1,20 +1,53 @@
 package com.leetcode.dp;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class No139WordBreak {
+
     public boolean wordBreak(String s, List<String> wordDict) {
-        final boolean[] f = new boolean[s.length() + 1];
-        f[0] = true;
+        return helper(s, wordDict, 0);
+    }
+
+    private boolean helper(String s, List<String> D, int start) {
+        if (start == s.length()) return true;
+
+        for (int i = start + 1; i <= s.length(); i++) {
+            if (D.contains(s.substring(start, i)) && helper(s, D, i)) return true;
+        }
+
+        return false;
+    }
+
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        final Set<String> set = new HashSet<>();
+        set.addAll(wordDict);
+
+        final boolean dp[] = new boolean[s.length()+1];
+        dp[0] = true;
+
+        for(int i = 0; i<s.length(); i++){
+            for(int j = i+1; j<=s.length();j++ ) if(dp[i] && set.contains(s.substring(i, j))) dp[j] = true;
+        }
+
+        return dp[dp.length-1];
+    }
+
+    public boolean wordBreak3(String s, List<String> wordDict) {
+        final boolean dp[] = new boolean[s.length() + 1];
+        dp[0] = true;
 
         for (int i = 1; i <= s.length(); i++) {
             for (String w : wordDict) {
-                if (f[i]) break;
-                int wLen = w.length();
-                if (i >= wLen && f[i - wLen] && w.equals(s.substring(i - wLen, i))) f[i] = true;
+                int wl = w.length(), start = i - wl;
+                if (dp[i] || start < 0 || !dp[start]) continue;
+
+                if (s.substring(start).startsWith(w)) dp[i] = true;
             }
         }
 
-        return f[s.length()];
+        return dp[dp.length - 1];
     }
+
 }
