d9dbb273e3a500f41401964c0b03ec0c389dc08a
Bofeng Li <bofeng.li@outlook.com>
2018-11-05 22:49:28 +0800

    LintCode: 586. Sqrt X II

---
+++ src/com/lintcode/math/No586SqrtXII.java
@@ -0,0 +1,36 @@
+package com.lintcode.math;
+
+public class No586SqrtXII {
+    /**
+     * @param x: a double
+     * @return: the square root of x
+     */
+    public double sqrt(double x) {
+        // Write your code here
+        double l = 0;
+        double r = Math.max(x, 1.0);
+        double eps = 1e-12;
+
+        while (l + eps < r) {
+            double mid = l + (r - l) / 2;
+            if (mid * mid < x) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+
+    public double sqrt2(double x) {
+        // write your code here
+        double epsilon = 1e-12;
+        double res = x;
+        while (Math.abs(res * res - x) > epsilon) {
+            res = (res + x / res) / 2;
+        }
+
+        return res;
+    }
+}
