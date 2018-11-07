2ae1ca4311651b54735585d87c4a5117f5d1c78b
Bofeng Li <bofeng.li@outlook.com>
2018-11-07 22:28:18 +0800

    LintCode: 895. Friend Request

--- src/com/lintcode/array/No895FriendRequest.java
+++ src/com/lintcode/array/No895FriendRequest.java
@@ -25,4 +25,22 @@ public class No895FriendRequest {
         return true;
     }
 
+
+    public int friendRequest2(int[] ages) {
+        int total = 0;
+        final int[] count = new int[151];
+        for (int age : ages) count[age]++;
+
+        for (int ageA = count.length - 1; ageA > 0; ageA--) {
+            int ageANum = count[ageA];
+
+            for (int ageB = ageA; ageB > ageA / 2 + 7; ageB--) {
+                if (ageA > 100 && ageB < 100) continue;
+
+                total += ageA == ageB ? ageANum * (ageANum - 1) : ageANum * count[ageB];
+            }
+        }
+
+        return total;
+    }
 }
