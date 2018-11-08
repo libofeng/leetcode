59004e7ad1ae49efdd9c0fa58b2724dc36166e2e
Bofeng Li <bofeng.li@outlook.com>
2018-11-08 19:15:55 +0800

    Update LeetCode LCA.

--- src/com/leetcode/tree/No236LowestCommonAncestorOfABinaryTree.java
+++ src/com/leetcode/tree/No236LowestCommonAncestorOfABinaryTree.java
@@ -1,15 +1,13 @@
 package com.leetcode.tree;
 
 public class No236LowestCommonAncestorOfABinaryTree {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if (root.val == p.val || root.val == q.val) return root;
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q), right = lowestCommonAncestor(root.right, p, q);
+    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lca(root.left, p, q), right = lca(root.right, p, q);
 
         if (left != null && right != null) return root;
-        else if (left != null) return left;
-        else if (right != null) return right;
-        else return null;
+        return left == null ? right : left;
     }
+
 }
