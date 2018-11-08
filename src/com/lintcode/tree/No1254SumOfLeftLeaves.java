3c36bc7cef17908df96d93c993a03da6b764ef4a
Bofeng Li <bofeng.li@outlook.com>
2018-11-08 21:06:30 +0800

    Update 1254. Sum of Left Leaves

--- src/com/lintcode/tree/No1254SumOfLeftLeaves.java
+++ src/com/lintcode/tree/No1254SumOfLeftLeaves.java
@@ -6,12 +6,26 @@ public class No1254SumOfLeftLeaves {
      * @return: the sum of all left leaves
      */
     public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) sum += root.left.val;
+            else sum += sumOfLeftLeaves(root.left);
+        }
+
+        if (root.right != null) sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+
+
+    public int sumOfLeftLeaves2(TreeNode root) {
         return helper(root, false);
     }
 
-    private int helper(TreeNode root, boolean isLeft){
-        if(root == null) return 0;
-        if(root.left == null && root.right ==null && isLeft) return root.val;
+    private int helper(TreeNode root, boolean isLeft) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null && isLeft) return root.val;
 
         int sum = 0;
         sum += helper(root.left, true);
