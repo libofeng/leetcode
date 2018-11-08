35417f3fc1725d8bef3ffdde0bdcea411ce1ad98
Bofeng Li <bofeng.li@outlook.com>
2018-11-08 20:09:11 +0800

    Update Hamming distance.

--- src/com/lintcode/bit/No835HammingDistance.java
+++ src/com/lintcode/bit/No835HammingDistance.java
@@ -11,4 +11,22 @@ public class No835HammingDistance {
         for (int i = 0; i < 32; i++) if (((x >> i) & 1) != ((y >> i) & 1)) count++;
         return count;
     }
+
+    public int hammingDistance2(int x, int y) {
+        int count = 0, xor = x ^ y;
+        for (int i = 0; i < 32; i++) count += (xor >> i) & 1;
+        return count;
+    }
+
+    public int hammingDistance3(int x, int y) {
+        int count = 0, xor = x ^ y;
+        while (xor > 0 && ++count > 0) xor &= xor - 1;
+        return count;
+    }
+
+    public int hammingDistance4(int x, int y) {
+        if ((x ^ y) == 0) return 0;
+        return ((x ^ y) & 1) + hammingDistance4(x >> 1, y >> 1);
+    }
+
 }
