13b5d8884f2ec33ce0d81a2184f8c64880554357
Bofeng Li <bofeng.li@outlook.com>
2018-11-08 22:44:59 +0800

    LintCode: 1350. Excel Sheet Column Title

---
+++ src/com/lintcode/math/No1350ExcelSheetColumnTitle.java
@@ -0,0 +1,22 @@
+package com.lintcode.math;
+
+public class No1350ExcelSheetColumnTitle {
+
+    /**
+     * @param n: a integer
+     * @return: return a string
+     */
+    public String convertToTitle(int n) {
+        return n-- == 0 ? "" : convertToTitle(n / 26) + (char) (n % 26 + 'A');
+    }
+
+    public String convertToTitle2(int n) {
+        final StringBuilder builder = new StringBuilder();
+        while (n-- > 0) {
+            builder.append((char) (n % 26 + 'A'));
+            n /= 26;
+        }
+
+        return builder.reverse().toString();
+    }
+}
