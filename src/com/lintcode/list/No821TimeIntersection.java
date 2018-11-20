0ff68e787699dd7dd5e743df61417569fbc883b7
Bofeng Li <bofeng.li@outlook.com>
2018-11-20 16:18:05 +0800

    Update 821. Time Intersection

--- src/com/lintcode/list/No821TimeIntersection.java
+++ src/com/lintcode/list/No821TimeIntersection.java
@@ -3,6 +3,8 @@ package com.lintcode.list;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class No821TimeIntersection {
 
@@ -11,6 +13,7 @@ public class No821TimeIntersection {
      * @param seqB: the list of intervals
      * @return: the time periods
      */
+    // Time complexity: O((A + B)),  Space complexity: O(A+B) OR O(1)
     public List<Interval> timeIntersection(List<Interval> seqA, List<Interval> seqB) {
         int i = 0, j = 0;
         final List<Interval> R = new LinkedList<>();
@@ -33,5 +36,32 @@ public class No821TimeIntersection {
         }
 
         return R;
+    }
+
+    // Sweep line. Time complexity: O((A + B)*LOG(A+B)),  Space complexity: O(A+B)
+    public List<Interval> timeIntersection2(List<Interval> seqA, List<Interval> seqB) {
+        final List<Interval> list = new LinkedList<>();
+
+        final Map<Integer, Integer> map = new TreeMap<>();
+        for(Interval i : seqA) {
+            map.put(i.start, map.getOrDefault(i.start, 0) + 1);
+            map.put(i.end, map.getOrDefault(i.end, 0) - 1);
+        }
+        for(Interval i : seqB) {
+            map.put(i.start, map.getOrDefault(i.start, 0) + 1);
+            map.put(i.end, map.getOrDefault(i.end, 0) - 1);
+        }
+
+        int count = 0, start = -1;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(count == 2 && start>=0){
+                list.add(new Interval(start, entry.getKey()));
+                start = -1;
+            }
+            count += entry.getValue();
+            if(count == 2) start = entry.getKey();
+        }
+
+        return list;
     }
 }
