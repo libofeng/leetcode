e65cd965bc0d25ab955d2b4228455a820f1cc653
Bofeng Li <bofeng.li@outlook.com>
2018-11-22 16:34:43 +0800

    Update No653ExpressionAddOperators.

--- src/com/lintcode/dfs/No653ExpressionAddOperators.java
+++ src/com/lintcode/dfs/No653ExpressionAddOperators.java
@@ -14,13 +14,14 @@ public class No653ExpressionAddOperators {
 
         for (int i = 1; i <= num.length(); i++) {
             long n = Long.parseLong(num.substring(0, i));
-            helper(num, target, R, i, n, "" + n);
+            helper(num, target, R, i, n, n, num.substring(0, i));
+            if (num.charAt(0) == '0') break;
         }
 
         return R;
     }
 
-    private void helper(String num, int target, List<String> R, int start, long expression, String path) {
+    private void helper(String num, int target, List<String> R, int start, long expression, long last, String path) {
         if (start == num.length()) {
             if (expression == target) R.add(path);
             return;
@@ -29,9 +30,10 @@ public class No653ExpressionAddOperators {
         for (int i = start + 1; i <= num.length(); i++) {
             long n = Long.parseLong(num.substring(start, i));
 
-            helper(num, target, R, i, expression - n, path + "-" + n);
-            helper(num, target, R, i, expression + n, path + "+" + n);
-            helper(num, target, R, i, expression * n, path + "*" + n);
+            helper(num, target, R, i, expression - n, -n, path + "-" + n);
+            helper(num, target, R, i, expression + n, n, path + "+" + n);
+            helper(num, target, R, i, expression - last + last * n, n * last, path + "*" + n);
+            if (num.charAt(start) == '0') break;
         }
     }
 }
