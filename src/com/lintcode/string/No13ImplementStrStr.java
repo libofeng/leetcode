e48516ce8f0fa2e389b6a71bf7ccba7ad8d4585c
Bofeng Li <bofeng.li@outlook.com>
2018-11-30 00:16:20 +0800

    Update 13. Implement strStr()

--- src/com/lintcode/string/No13ImplementStrStr.java
+++ src/com/lintcode/string/No13ImplementStrStr.java
@@ -7,15 +7,12 @@ public class No13ImplementStrStr {
      * @return: return the index
      */
     public int strStr(String source, String target) {
-        if (target.length() == 0) return 0;
+        if (target.isEmpty()) return 0;
 
-        final int ls = source.length(), lt = target.length();
-        for (int i = 0; i <= ls - lt; i++) {
-            if (source.charAt(i) != target.charAt(0)) continue;
-
+        for (int i = 0; i <= source.length() - target.length(); i++) {
             int j = 0;
-            for (; j < lt; j++) if (source.charAt(i + j) != target.charAt(j)) break;
-            if (j == lt) return i;
+            for (; j < target.length(); j++) if (source.charAt(i + j) != target.charAt(j)) break;
+            if (j == target.length()) return i;
         }
 
         return -1;
