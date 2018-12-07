0bec6c8e74dc028c6c9e2fe0b2b6689eb753c5fb
Bofeng Li <bofeng.li@outlook.com>
2018-12-07 22:36:25 +0800

    Facebook interview: binary tree to doubly linked list.

---
+++ src/com/facebook/BinaryTreeToDoublyLinkedList.java
@@ -0,0 +1,29 @@
+package com.facebook;
+
+
+public class BinaryTreeToDoublyLinkedList {
+    TreeNode head, last;
+
+    public TreeNode build(TreeNode root) {
+        inorder(root);
+        return head;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        link(node);
+        inorder(node.right);
+    }
+
+
+    private void link(TreeNode node) {
+        if (node == null) return;
+
+        node.left = last;
+        if (last == null) head = node;
+        else last.right = node;
+
+        last = node;
+    }
+}
---
+++ src/com/facebook/TreeNode.java
@@ -0,0 +1,11 @@
+package com.facebook;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
