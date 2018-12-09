e9775ff595c62e0ba8b912452b63671ab10fcbda
Bofeng Li <bofeng.li@outlook.com>
2018-12-09 11:02:38 +0800

    Update: The Skyline Problem

--- src/com/lintcode/array/No131TheSkylineProblem.java
+++ src/com/lintcode/array/No131TheSkylineProblem.java
@@ -92,4 +92,37 @@ public class No131TheSkylineProblem {
         }
         return res;
     }
+
+    public List<List<Integer>> buildingOutline2(int[][] buildings) {
+        List<int[]> list = new ArrayList<>();
+        for (int[] b : buildings) {
+            list.add(new int[]{b[0], b[2]});
+            list.add(new int[]{b[1], -b[2]});
+        }
+        list.sort((a, b) -> a[0] == b[0] ? (b[1] - a[1]) : (a[0] - b[0]));
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        int start = 0, max = 0;
+        List<List<Integer>> R = new LinkedList<>();
+        for (int[] point : list) {
+            int x = point[0], h = point[1];
+            if (h > 0) map.put(h, map.getOrDefault(h, 0) + 1);
+            else {
+                h = -h;
+                if (map.get(h) == 1) map.remove(h);
+                else map.put(h, map.get(h) - 1);
+            }
+
+            int currentMax = map.isEmpty() ? 0 : map.lastKey();
+            if (currentMax != max) {
+                if (x != start && max != 0) R.add(Arrays.asList(start, x, max));
+
+                max = currentMax;
+                start = x;
+            }
+        }
+
+        return R;
+    }
 }
