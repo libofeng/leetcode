5b8a722346474a1229dd6670866d81c1ba2b5eec
Bofeng Li <bofeng.li@outlook.com>
2018-12-14 17:10:53 +0800

    236. Lowest Common Ancestor of a Binary Tree

--- src/com/leetcode/tree/No236LowestCommonAncestorOfABinaryTree.java
+++ src/com/leetcode/tree/No236LowestCommonAncestorOfABinaryTree.java
@@ -1,6 +1,7 @@
 package com.leetcode.tree;
 
 public class No236LowestCommonAncestorOfABinaryTree {
+    // https://segmentfault.com/a/1190000009429876
 
     public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root.val == p.val || root.val == q.val) return root;
@@ -8,6 +9,17 @@ public class No236LowestCommonAncestorOfABinaryTree {
 
         if (left != null && right != null) return root;
         return left == null ? right : left;
+    }
+
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        if (left != null && left.val != p.val && left.val != q.val) return left; // optimization
+
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+
+        return left != null && right != null ? root : (right == null ? left : right);
     }
 
 }
