48af71e705b16fa54f25235af858dac7df43a27a
Bofeng Li <bofeng.li@outlook.com>
2018-12-14 08:37:51 +0800

    LintCode: simplify 475. Binary Tree Maximum Path Sum II.

--- src/com/lintcode/tree/No475BinaryTreeMaximumPathSumII.java
+++ src/com/lintcode/tree/No475BinaryTreeMaximumPathSumII.java
@@ -1,13 +1,23 @@
 package com.lintcode.tree;
 
 public class No475BinaryTreeMaximumPathSumII {
+
+
     /**
      * @param root: the root of binary tree.
      * @return: An integer
      */
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxPathSum2(root.left), right = maxPathSum2(root.right);
+        return Math.max(Math.max(root.val, root.val + left), root.val + right);
+    }
+
+
     int max = Integer.MIN_VALUE;
 
-    public int maxPathSum2(TreeNode root) {
+    public int maxPathSum21(TreeNode root) {
         if (root == null) return 0;
 
         preorder(root, 0);
