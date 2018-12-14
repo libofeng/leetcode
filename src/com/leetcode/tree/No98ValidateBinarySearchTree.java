fadf9d895dba48af1cc100e50867ad11408e3475
Bofeng Li <bofeng.li@outlook.com>
2018-12-14 15:35:18 +0800

    Update 114. Flatten Binary Tree to Linked List.

--- src/com/leetcode/tree/No98ValidateBinarySearchTree.java
+++ src/com/leetcode/tree/No98ValidateBinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.leetcode.tree;
 
+import java.util.Stack;
+
 public class No98ValidateBinarySearchTree {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
@@ -10,5 +12,43 @@ public class No98ValidateBinarySearchTree {
         if (root.val <= low || root.val >= high) return false;
 
         return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
+    }
+
+
+    TreeNode prev = null;
+
+    public boolean isValidBST2(TreeNode root) {
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode root) {
+        if (root == null) return true;
+        if (!inorder(root.left)) return false;
+
+        if (prev != null && prev.val >= root.val) return false;
+        prev = root;
+
+        return inorder(root.right);
+    }
+
+    public boolean isValidBST3(TreeNode root) {
+        if (root == null) return true;
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root, prev = null;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            if (prev != null && prev.val >= current.val) return false;
+            prev = current;
+            current = current.right;
+        }
+
+        return true;
     }
 }
