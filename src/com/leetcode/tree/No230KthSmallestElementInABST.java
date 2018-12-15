45b57e4d146720e5f0e7be2d1e38a937c0c16ffd
Bofeng Li <bofeng.li@outlook.com>
2018-12-15 10:33:22 +0800

    Update 230. Kth Smallest Element in a BST

--- src/com/leetcode/tree/No230KthSmallestElementInABST.java
+++ src/com/leetcode/tree/No230KthSmallestElementInABST.java
@@ -1,5 +1,7 @@
 package com.leetcode.tree;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class No230KthSmallestElementInABST {
@@ -21,5 +23,24 @@ public class No230KthSmallestElementInABST {
         }
 
         return -1;
+    }
+
+    // ---------------------------
+
+
+    Map<TreeNode, Integer> cache = new HashMap<>();
+
+    public int kthSmallest2(TreeNode root, int k) {
+        int c = count(root.left);
+        if (c + 1 == k) return root.val;
+        return k <= c ? kthSmallest(root.left, k) : kthSmallest(root.right, k - c - 1);
+    }
+
+    private int count(TreeNode node) {
+        if (node == null) return 0;
+
+        int c = count(node.left) + count(node.right) + 1;
+        cache.put(node, c);
+        return c;
     }
 }
