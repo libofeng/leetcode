5092c0c9a9fbcd36d3a122b49158fd9d036eebe8
Bofeng Li <bofeng.li@outlook.com>
2018-12-16 09:09:59 +0800

    LintCode: 528. Flatten Nested List Iterator

--- src/com/lintcode/list/No528FlattenNestedListIterator.java
+++ src/com/lintcode/list/No528FlattenNestedListIterator.java
@@ -1,4 +1,39 @@
 package com.lintcode.list;
 
-public class No528FlattenNestedListIterator {
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No528FlattenNestedListIterator implements Iterator<Integer> {
+
+    private Deque<NestedInteger> dq = new LinkedList<>();
+
+    public No528FlattenNestedListIterator(List<NestedInteger> nestedList) {
+        for (NestedInteger i : nestedList) dq.offerLast(i);
+    }
+
+    // @return {int} the next element in the iteration
+    @Override
+    public Integer next() {
+        return hasNext() ? dq.poll().getInteger() : null;
+    }
+
+    private void flatten() {
+        while (!dq.isEmpty() && !dq.peek().isInteger()) {
+            List<NestedInteger> list = dq.poll().getList();
+            for (int i = list.size() - 1; i >= 0; i--) dq.offerFirst(list.get(i));
+        }
+    }
+
+    // @return {boolean} true if the iteration has more element or false
+    @Override
+    public boolean hasNext() {
+        flatten();
+        return !dq.isEmpty();
+    }
+
+    @Override
+    public void remove() {
+    }
 }
