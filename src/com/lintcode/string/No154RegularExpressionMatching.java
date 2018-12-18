697d347dac8b85879e591962b4c45dfae8fdc294
Bofeng Li <bofeng.li@outlook.com>
2018-12-18 22:55:34 +0800

    Update Regular Expression Matching.

--- src/com/lintcode/string/No154RegularExpressionMatching.java
+++ src/com/lintcode/string/No154RegularExpressionMatching.java
@@ -10,46 +10,24 @@ public class No154RegularExpressionMatching {
         return isMatch(s, 0, p, 0);
     }
 
+    private boolean matchFirst(String s, int i, String p, int j) {
+        if (i == s.length()) return j == p.length();
+        if (j == p.length()) return i == s.length();
+
+        return s.charAt(i) == p.charAt(j) || '.' == p.charAt(j);
+    }
+
     private boolean isMatch(String s, int i, String p, int j) {
-        if (j == p.length()) return s.length() == i;
-        if (j + 1 == p.length()) {
-            return i + 1 == s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
-        }
+        if (j == p.length()) return i == s.length();
 
-        if (p.charAt(j + 1) != '*') {
-            if (i == s.length()) return false;
-            return (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s, i + 1, p, j + 1);
-        }
-
-        while (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
+        if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
             if (isMatch(s, i, p, j + 2)) return true;
-            i++;
+            while (matchFirst(s, i, p, j)) if (isMatch(s, ++i, p, j + 2)) return true;
+            return false;
         }
 
-        return isMatch(s, i, p, j + 2);
+        return matchFirst(s, i, p, j) && isMatch(s, i + 1, p, j + 1);
     }
-
-    private boolean isMatch2(String s, int i, String p, int j) {
-        final int m = s.length(), n = p.length();
-        if (j == n) return i == m;
-        if (j < n - 1 && p.charAt(j + 1) == '*') {
-            return isMatch(s, i, p, j + 2)
-                    || (i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s, i + 1, p, j));
-        }
-
-        return i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s, i + 1, p, j + 1);
-    }
-
-    private boolean isMatch2(char[] s, int i, char[] p, int j) {
-        final int m = s.length, n = p.length;
-        if (j == n) return i == m;
-        if (j < n - 1 && p[j + 1] == '*') {
-            return isMatch2(s, i, p, j + 2) || (i < m && (s[i] == p[j] || p[j] == '.') && isMatch2(s, i + 1, p, j));
-        }
-
-        return i < m && (s[i] == p[j] || p[j] == '.') && isMatch2(s, i + 1, p, j + 1);
-    }
-
 
     public boolean isMatch2(String s, String p) {
         final int m = s.length(), n = p.length();
