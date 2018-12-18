7497b19c62a28ec0b063750e71cdffe5ec6206fd
Bofeng Li <bofeng.li@outlook.com>
2018-12-18 14:18:40 +0800

    TopologicalSort

---
+++ src/com/TopologicalSort.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class TopologicalSort {
+    /**
+     * Get topological ordering of the input directed graph
+     *
+     * @param n             number of nodes in the graph
+     * @param adjacencyList adjacency list representation of the input directed graph
+     * @return topological ordering of the graph stored in an List<Integer>.
+     */
+    public List<Integer> topologicalSort(int n, int[][] adjacencyList) {
+        List<Integer> topoRes = new ArrayList<>();
+        int[] inDegree = new int[n];
+        for (int[] parent : adjacencyList) {
+            for (int child : parent) {
+                inDegree[child]++;
+            }
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // start from nodes whose indegree are 0
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) deque.offer(i);
+        }
+
+        while (!deque.isEmpty()) {
+            int curr = deque.poll();
+            topoRes.add(curr);
+            for (int child : adjacencyList[curr]) {
+                inDegree[child]--;
+                if (inDegree[child] == 0) {
+                    deque.offer(child);
+                }
+            }
+        }
+
+        return topoRes.size() == n ? topoRes : new ArrayList<>();
+    }
+}
