5dfecf7477dbcf7cc69385c3788f702472b6ca0f
Bofeng Li <bofeng.li@outlook.com>
2018-12-19 08:02:01 +0800

    Update No428PowXN.

--- src/com/lintcode/math/No428PowXN.java
+++ src/com/lintcode/math/No428PowXN.java
@@ -9,7 +9,6 @@ public class No428PowXN {
     public double myPow(double x, int n) {
         if (n == 0) return 1;
         if (n < 0) return (1 / x) * myPow(1 / x, -(n + 1));
-        if (n < 0) return (1 / x) * myPow(1 / x, -n - 1);
 
         return (n & 1) == 0 ? myPow(x * x, n / 2) : x * myPow(x, n - 1);
     }
