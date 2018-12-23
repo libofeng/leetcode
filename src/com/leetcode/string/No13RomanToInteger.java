eca513cc54270f62af6a37140997c73af4a5b78f
Bofeng Li <bofeng.li@outlook.com>
2018-12-24 07:50:01 +0800

    13. Roman to Integer

--- src/com/leetcode/string/No13RomanToInteger.java
+++ src/com/leetcode/string/No13RomanToInteger.java
@@ -22,4 +22,35 @@ public class No13RomanToInteger {
 
         return R;
     }
+
+    public int romanToInt2(String s) {
+        int num = 0, len = s.length();
+        for (int i = 0; i < len; i++) {
+            int n = map(s.charAt(i));
+            num += i < len - 1 && n < map(s.charAt(i + 1)) ? -n : n;
+        }
+
+        return num;
+    }
+
+    private int map(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
 }
