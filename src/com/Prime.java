c6df2e455f480b00c60e3177ed3d7dd3b72f9714
Bofeng Li <bofeng.li@outlook.com>
2018-12-25 02:41:31 +0800

    Prime.

---
+++ src/com/Prime.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Prime {
+    private boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
+        return true;
+    }
+}
