4be4be93bf5414da4e0dd88db65ce8f3d69e357a
Bofeng Li <bofeng.li@outlook.com>
2018-12-24 21:29:10 +0800

    Update 66. Plus One

--- src/com/leetcode/array/No66PlusOne.java
+++ src/com/leetcode/array/No66PlusOne.java
@@ -17,4 +17,16 @@ public class No66PlusOne {
 
         return newDigits;
     }
+
+
+    public int[] plusOne2(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i] = digits[i] == 9 ? 0 : digits[i] + 1;
+            if (digits[i] != 0) return digits;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
 }
