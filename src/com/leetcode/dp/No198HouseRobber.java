c86f73ef847825c4603722bc54a0785f2af1e883
Bofeng Li <bofeng.li@outlook.com>
2018-12-25 23:33:03 +0800

    198. House Robber

--- src/com/leetcode/dp/No198HouseRobber.java
+++ src/com/leetcode/dp/No198HouseRobber.java
@@ -8,4 +8,18 @@ public class No198HouseRobber {
         return dp[dp.length - 1];
     }
 
+
+    public int rob2(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int left = nums[0], right = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int max = Math.max(left + nums[i], right);
+            left = right;
+            right = max;
+        }
+
+        return right;
+    }
 }
