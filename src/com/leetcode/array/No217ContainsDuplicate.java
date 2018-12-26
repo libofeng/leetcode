528728c401aaed57d17d1f03f95e2c6e9c7c8540
Bofeng Li <bofeng.li@outlook.com>
2018-12-26 23:33:26 +0800

    217. Contains Duplicate

--- src/com/leetcode/array/No217ContainsDuplicate.java
+++ src/com/leetcode/array/No217ContainsDuplicate.java
@@ -5,22 +5,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class No217ContainsDuplicate {
-    // sorting
+    // Sorting:
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) if (nums[i] == nums[i - 1]) return true;
-
         return false;
     }
 
-    // set
+    // Using HashSet:
     public boolean containsDuplicate2(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) return true;
-            set.add(num);
-        }
-
+        for (int n : nums) if (!set.add(n)) return true;
         return false;
     }
+
 }
