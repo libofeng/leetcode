bd5d8b3402978046efcb0aaf9a8e229597ac4fd0
Bofeng Li <bofeng.li@outlook.com>
2018-12-26 22:34:07 +0800

    461. Hamming Distance

---
+++ src/com/leetcode/bit/No461HammingDistance.java
@@ -0,0 +1,21 @@
+package com.leetcode.bit;
+
+public class No461HammingDistance {
+    public int hammingDistance(int x, int y) {
+        if ((x ^ y) == 0) return 0;
+        int count = (x & 1) == (y & 1) ? 0 : 1;
+        return count + hammingDistance(x >> 1, y >> 1);
+    }
+
+
+    public int hammingDistance2(int x, int y) {
+        return (x ^ y) == 0 ? 0 : (((x & 1) ^ (y & 1)) + hammingDistance(x >> 1, y >> 1));
+    }
+
+
+    public int hammingDistance3(int x, int y) {
+        int n = x ^ y, distance = 0;
+        for (int i = 0; i < 32; i++) if (((n >> 1) & 1) == 1) distance++;
+        return distance;
+    }
+}
