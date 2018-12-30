c2e5a339c156f28163ae9d449d219c83cae51d55
Bofeng Li <bofeng.li@outlook.com>
2018-12-30 22:23:47 +0800

    247.Strobogrammatic Number II

--- src/com/leetcode/string/No247StrobogrammaticNumberII.java
+++ src/com/leetcode/string/No247StrobogrammaticNumberII.java
@@ -6,36 +6,43 @@ import java.util.List;
 
 public class No247StrobogrammaticNumberII {
     public List<String> findStrobogrammatic(int n) {
-        List<String> list = n % 2 == 1 ? Arrays.asList("1", "8", "0") : Arrays.asList("");
-        if (n <= 1) return list;
+        return find(n, n);
+    }
 
-        final List<String> R = new ArrayList<>();
-        for (String path : list) R.addAll(getNumber(n, path));
+    private List<String> find(int n, int left) {
+        if (left == 0) return Arrays.asList("");
+        if (left == 1) return Arrays.asList("0", "1", "8");
+
+        List<String> list = find(n, left - 2);
+        List<String> R = new ArrayList<>();
+        for (String l : list) {
+            if (left != n) R.add('0' + l + '0');
+            R.add('1' + l + '1');
+            R.add('6' + l + '9');
+            R.add('8' + l + '8');
+            R.add('9' + l + '6');
+        }
 
         return R;
     }
 
-    private List<String> getNumber(int n, String path) {
-        char[] chars = {'0', '1', '6', '8', '9'};
-        List<String> numbers = new ArrayList<>();
-        dfs(chars, numbers, n, 0, path);
-        return numbers;
-    }
 
-    private void dfs(char[] chars, List<String> numbers, int n, int start, String path) {
-        if (path.length() == n) {
-            if (n == 1 || path.charAt(0) != '0') numbers.add(path);
-            return;
+    public List<String> findStrobogrammatic2(int n) {
+        List<String> list = n % 2 == 1 ? Arrays.asList("1", "8", "0") : Arrays.asList("");
+        if (n <= 1) return list;
+
+        for (int i = (n % 2) + 2; i <= n; i += 2) {
+            List<String> tmp = new ArrayList<>();
+            for (String s : list) {
+                if (i != n) tmp.add('0' + s + '0');
+                tmp.add('1' + s + '1');
+                tmp.add('6' + s + '9');
+                tmp.add('8' + s + '8');
+                tmp.add('9' + s + '6');
+            }
+            list = tmp;
         }
 
-        for (int i = 0; i < chars.length; i++) {
-            dfs(chars, numbers, n, i, chars[i] + path + rotate69(chars[i]));
-        }
-    }
-
-    private char rotate69(char c) {
-        if (c == '9') return '6';
-        if (c == '6') return '9';
-        return c;
+        return list;
     }
 }
