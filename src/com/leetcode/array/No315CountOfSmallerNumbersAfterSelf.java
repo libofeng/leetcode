ae10ce18a52729b1e234d415f0a2fea79865cb96
Bofeng Li <bofeng.li@outlook.com>
2019-01-02 21:22:55 +0800

    Update 315. Count of Smaller Numbers After Self.

--- src/com/leetcode/array/No315CountOfSmallerNumbersAfterSelf.java
+++ src/com/leetcode/array/No315CountOfSmallerNumbersAfterSelf.java
@@ -6,36 +6,45 @@ import java.util.List;
 public class No315CountOfSmallerNumbersAfterSelf {
     class Node {
         Node left, right;
-        int val, sum, dup = 1;
+        int val, totalCount, count;
+        // count: total elements eq val
+        // totalCount: total elements in the left subtree(including duplication)
 
-        public Node(int v, int s) {
+        Node(int v) {
             val = v;
-            sum = s;
+            count = 1;
         }
     }
+
 
     public List<Integer> countSmaller(int[] nums) {
-        Integer[] ans = new Integer[nums.length];
+        Integer[] result = new Integer[nums.length];
+
         Node root = null;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            root = insert(nums[i], root, ans, i, 0);
-        }
-        return Arrays.asList(ans);
+        for (int i = nums.length - 1; i >= 0; i--) root = insert(root, nums[i], result, i, 0);
+        return Arrays.asList(result);
     }
 
-    private Node insert(int num, Node node, Integer[] ans, int i, int preSum) {
+    private Node insert(Node node, int val, Integer[] result, int i, int preTotal) {
         if (node == null) {
-            node = new Node(num, 0);
-            ans[i] = preSum;
-        } else if (node.val == num) {
-            node.dup++;
-            ans[i] = preSum + node.sum;
-        } else if (node.val > num) {
-            node.sum++;
-            node.left = insert(num, node.left, ans, i, preSum);
+            node = new Node(val);
+            result[i] = preTotal;
+        } else if (node.val == val) {
+            node.count++;
+            result[i] = preTotal + node.totalCount;
+        } else if (node.val > val) {
+            node.totalCount++;
+            node.left = insert(node.left, val, result, i, preTotal);
         } else {
-            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+            node.right = insert(node.right, val, result, i, preTotal + node.count + node.totalCount);
         }
         return node;
+    }
+
+
+    public static void main(String[] args) {
+        No315CountOfSmallerNumbersAfterSelf solution = new No315CountOfSmallerNumbersAfterSelf();
+        List<Integer> result = solution.countSmaller(new int[]{5, 2, 6, 1});
+        System.out.println("result = " + result);
     }
 }
