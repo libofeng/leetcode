1a2e1b29df379d9b67bf1fce03c454ffdd4a54ad
Bofeng Li <bofeng.li@outlook.com>
2019-01-06 09:03:58 +0800

    Update No315CountOfSmallerNumbersAfterSelf by using BIT.

--- src/com/leetcode/array/No315CountOfSmallerNumbersAfterSelf.java
+++ src/com/leetcode/array/No315CountOfSmallerNumbersAfterSelf.java
@@ -41,6 +41,49 @@ public class No315CountOfSmallerNumbersAfterSelf {
         return node;
     }
 
+    // BIT
+
+    public List<Integer> countSmaller3(int[] nums) {
+        final int n = nums.length;
+        final Integer[] result = new Integer[nums.length];
+        if(nums.length == 0) return Arrays.asList(result);
+
+        int[] copy = nums.clone();
+        Arrays.sort(copy);
+
+        final int[] BIT = new int[n + 1];
+        for(int i = n - 1;i >= 0; i--){
+            int index = findIndex(copy, nums[i]);
+            result[i] = query(BIT, index);
+            update(BIT, index);
+        }
+
+        return Arrays.asList(result);
+    }
+
+    private int findIndex(int[] nums, int num){
+        int lo = 0, hi = nums.length - 1;
+        while(lo<hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if(num <= nums[mid]) hi = mid;
+            else lo = mid + 1;
+        }
+
+        return lo;
+    }
+
+    private void update(int[] BIT, int index){
+        for(int i = index + 1; i<BIT.length; i += (i & -i)) BIT[i] += 1;
+    }
+
+    private int query(int[] BIT, int index){
+        int count = 0;
+        for(int i = index; i>0;i -= (i & -i)) count += BIT[i];
+
+        return count;
+    }
+
 
     public static void main(String[] args) {
         No315CountOfSmallerNumbersAfterSelf solution = new No315CountOfSmallerNumbersAfterSelf();
