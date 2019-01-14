54b3128775c3890f4e37472928cd4b02a32ab89c
Bofeng Li <bofeng.li@outlook.com>
2019-01-14 10:03:50 +0800

    Update No647PalindromicSubstrings

--- src/com/leetcode/string/No647PalindromicSubstrings.java
+++ src/com/leetcode/string/No647PalindromicSubstrings.java
@@ -1,23 +1,17 @@
 package com.leetcode.string;
 
 public class No647PalindromicSubstrings {
-    int count = 0;
+    int count;
 
     public int countSubstrings(String s) {
         for (int i = 0; i < s.length(); i++) {
             expand(s, i, i);
             expand(s, i, i + 1);
         }
-
         return count;
     }
 
-    private void expand(String s, int i1, int i2) {
-        while (i1 >= 0 && i2 < s.length() && s.charAt(i1) == s.charAt(i2)) {
-            count++;
-
-            i2++;
-            i1--;
-        }
+    private void expand(String s, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i--) == s.charAt(j++)) count++;
     }
 }
