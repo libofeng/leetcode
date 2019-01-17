62c21f18c32a86eadd04d59c7225126424ce9c97
Bofeng Li <bofeng.li@outlook.com>
2019-01-17 23:53:06 +0800

    10. Regular Expression Matching

--- src/com/leetcode/string/No10RegularExpressionMatching.java
+++ src/com/leetcode/string/No10RegularExpressionMatching.java
@@ -39,4 +39,24 @@ public class No10RegularExpressionMatching {
             return false;
         }
     }
+
+
+    // DP
+    public boolean isMatch3(String s, String p) {
+        final int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j > 1 && p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || '.' == p.charAt(j - 2)));
+                } else {
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || '.' == p.charAt(j - 1));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 }
