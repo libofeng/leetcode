597ee5f4d3ca6257dca05ee9e9f671e66fe6537d
Bofeng Li <bofeng.li@outlook.com>
2019-01-18 16:24:47 +0800

    65. Valid Number

--- src/com/leetcode/string/No65ValidNumber.java
+++ src/com/leetcode/string/No65ValidNumber.java
@@ -4,26 +4,25 @@ public class No65ValidNumber {
     public boolean isNumber(String s) {
         s = s.trim();
 
-        boolean pointSeen = false;
-        boolean eSeen = false;
-        boolean numberSeen = false;
-        boolean numberAfterE = true;
+        boolean pointSeen = false, eSeen = false, numberSeen = false, numberAfterE = false;
         for (int i = 0; i < s.length(); i++) {
-            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
                 numberSeen = true;
                 numberAfterE = true;
-            } else if (s.charAt(i) == '.') {
+            } else if (c == '.') {
                 if (eSeen || pointSeen) {
                     return false;
                 }
                 pointSeen = true;
-            } else if (s.charAt(i) == 'e') {
+            } else if (c == 'e') {
                 if (eSeen || !numberSeen) {
                     return false;
                 }
                 numberAfterE = false;
                 eSeen = true;
-            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+            } else if (c == '-' || c == '+') {
                 if (i != 0 && s.charAt(i - 1) != 'e') {
                     return false;
                 }
