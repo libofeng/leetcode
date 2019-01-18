4915004c52311f28a44aedf9a9e0fb8d37433eef
Bofeng Li <bofeng.li@outlook.com>
2019-01-18 15:52:12 +0800

    8. String to Integer (atoi)

--- src/com/leetcode/string/No8StringToIntegerAtoi.java
+++ src/com/leetcode/string/No8StringToIntegerAtoi.java
@@ -52,27 +52,25 @@ public class No8StringToIntegerAtoi {
 
 
     public int myAtoi2(String str) {
-        boolean signFound = false;
         int sign = 1;
         long num = 0;
+        boolean signFound = false;
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
+
             if (c == ' ') {
                 if (signFound) break;
-                else continue;
-            }
-
-            if (c == '-' || c == '+') {
+            } else if (c == '+' || c == '-') {
                 if (signFound) break;
-                else sign = c == '-' ? -1 : 1;
                 signFound = true;
-            } else if (c >= '0' && c <= '9') {
+                if (c == '-') sign = -1;
+            } else if (Character.isDigit(c)) {
                 signFound = true;
 
                 num = num * 10 + (c - '0') * sign;
-
-                if (num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
                 if (num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+                if (num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
             } else break;
         }
 
