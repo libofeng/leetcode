34b00a579d45ed24f44bfc96f4ef770908a86a98
Bofeng Li <bofeng.li@outlook.com>
2019-01-19 23:38:24 +0800

    Update 66. Plus One

--- src/com/leetcode/array/No66PlusOne.java
+++ src/com/leetcode/array/No66PlusOne.java
@@ -3,19 +3,16 @@ package com.leetcode.array;
 public class No66PlusOne {
     public int[] plusOne(int[] digits) {
         int carry = 1;
-
         for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            digits[i] = sum % 10;
-            carry = sum / 10;
-            if (carry == 0) return digits;
+            int n = digits[i] + carry;
+            digits[i] = n % 10;
+            carry = n / 10;
         }
 
-        int[] newDigits = new int[digits.length + 1];
-        System.arraycopy(digits, 0, newDigits, 1, digits.length);
-        newDigits[0] = 1;
-
-        return newDigits;
+        if (carry == 0) return digits;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 
 
