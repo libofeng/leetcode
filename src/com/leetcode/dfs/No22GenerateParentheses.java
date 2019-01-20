f16aec0d389573cdbecbbb0fba396ddfdcb9103e
Bofeng Li <bofeng.li@outlook.com>
2019-01-20 20:49:00 +0800

    22. Generate Parentheses

--- src/com/leetcode/dfs/No22GenerateParentheses.java
+++ src/com/leetcode/dfs/No22GenerateParentheses.java
@@ -1,33 +1,20 @@
 package com.leetcode.dfs;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class No22GenerateParentheses {
     public List<String> generateParenthesis(int n) {
-        final List<String> R = new LinkedList<>();
-
-        dfs(R, n, n, new StringBuilder());
-
-        return R;
+        List<String> result = new ArrayList<>();
+        dfs(n, n, result, "");
+        return result;
     }
 
-    private void dfs(List<String> R, int left, int right, StringBuilder builder) {
-        if (left == 0 && right == 0) {
-            R.add(builder.toString());
-            return;
-        }
-
-        if (left > 0) {
-            builder.append("(");
-            dfs(R, left - 1, right, builder);
-            builder.deleteCharAt(builder.length() - 1);
-        }
-
-        if (right > 0 && left < right) {
-            builder.append(")");
-            dfs(R, left, right - 1, builder);
-            builder.deleteCharAt(builder.length() - 1);
+    private void dfs(int left, int right, List<String> result, String s) {
+        if (left == 0 && right == 0) result.add(s);
+        else {
+            if (left > 0) dfs(left - 1, right, result, s + "(");
+            if (right > left) dfs(left, right - 1, result, s + ")");
         }
     }
 }
