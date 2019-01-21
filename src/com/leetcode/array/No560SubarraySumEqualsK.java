d42ce12430a18f9bf5fda0cc1a08e7c55794d0b7
Bofeng Li <bofeng.li@outlook.com>
2019-01-21 22:03:19 +0800

    560. Subarray Sum Equals K

--- src/com/leetcode/array/No560SubarraySumEqualsK.java
+++ src/com/leetcode/array/No560SubarraySumEqualsK.java
@@ -4,15 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class No560SubarraySumEqualsK {
+    // 1. prefix sum: this is based on Kadane's Algorithm
+    // 2. set initiative state 0:1, means there is 1 sum of 0
+
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> sumMap = new HashMap<>();
+        final Map<Integer, Integer> sumMap = new HashMap<>();
         sumMap.put(0, 1);
 
         int sum = 0, total = 0;
         for (int n : nums) {
             sum += n;
-            int targetSum = sum - k;
-            if (sumMap.containsKey(targetSum)) total += sumMap.get(targetSum);
+            total += sumMap.getOrDefault(sum - k, 0);
             sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
         }
 
