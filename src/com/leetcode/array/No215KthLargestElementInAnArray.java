c2a9ebef30ecc020d7ad3f28e3fd61b98a913f59
Bofeng Li <bofeng.li@outlook.com>
2019-01-21 15:59:35 +0800

    215. Kth Largest Element in an Array

--- src/com/leetcode/array/No215KthLargestElementInAnArray.java
+++ src/com/leetcode/array/No215KthLargestElementInAnArray.java
@@ -21,24 +21,21 @@ public class No215KthLargestElementInAnArray {
     private int quickSelect(int[] nums, int start, int end, int k) {
         int pIndex = partition(nums, start, end);
 
-        int rightNumbers = end - pIndex + 1;
-        if (rightNumbers == k) return nums[pIndex];
-        else if (rightNumbers > k) return quickSelect(nums, pIndex + 1, end, k);
-        else return quickSelect(nums, start, pIndex - 1, k - rightNumbers);
+        int right = end - pIndex + 1;
+        if (right == k) return nums[pIndex];
+        else if (right > k) return quickSelect(nums, pIndex + 1, end, k);
+        else return quickSelect(nums, start, pIndex - 1, k - right);
     }
 
     private int partition(int[] nums, int start, int end) {
-        Random rnd = new Random();
-        int pIndex = rnd.nextInt(end - start + 1) + start;
-        swap(nums, pIndex, end);
-
-        int left = start, right = end - 1, pivot = nums[end];
-        while (left <= right) {
-            if (nums[right] > pivot) right--;
-            else swap(nums, left++, right);
+        int lo = start, hi = end - 1, pivot = nums[end];
+        while (lo <= hi) {
+            if (nums[lo] < pivot) lo++;
+            else swap(nums, lo, hi--);
         }
-        swap(nums, left, end);
-        return left;
+
+        swap(nums, lo, end);
+        return lo;
     }
 
     private void swap(int[] nums, int i, int j) {
@@ -51,20 +48,14 @@ public class No215KthLargestElementInAnArray {
 
     // https://github.com/interviewdiscussion/files/blob/master/Facebook_java%2Bpdf/215.%20Kth%20Largest%20Element%20in%20an%20Array.java
     public int findKthLargest3(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k < 1) {
-            return 0;
+        if (nums == null || nums.length == 0 || k < 1) return 0;
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            if (pq.size() < k || n > pq.peek()) pq.offer(n);
+            if (pq.size() > k) pq.poll();
         }
-        Queue<Integer> minHeap = new PriorityQueue<>();
-        for (int i : nums) {
-            if (minHeap.size() < k) {
-                minHeap.offer(i);
-                continue;
-            }
-            if (i > minHeap.peek()) {
-                minHeap.poll();
-                minHeap.offer(i);
-            }
-        }
-        return minHeap.poll();
+
+        return pq.peek();
     }
 }
