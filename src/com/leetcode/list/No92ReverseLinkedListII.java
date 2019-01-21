6cb60cdb71f629dd1ac74e326b7c31ecdc2568e2
Bofeng Li <bofeng.li@outlook.com>
2019-01-21 12:53:57 +0800

    92. Reverse Linked List II

--- src/com/leetcode/list/No92ReverseLinkedListII.java
+++ src/com/leetcode/list/No92ReverseLinkedListII.java
@@ -1,18 +1,25 @@
 package com.leetcode.list;
 
 public class No92ReverseLinkedListII {
-    public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null || m == n) return head;
 
-        ListNode dummyHead = new ListNode(0), current = head;
-        while(current!=null){
-            ListNode node = current;
-            current = current.next;
+        ListNode dummyHead = new ListNode(0), prev = dummyHead;
+        dummyHead.next = head;
 
-            node.next = dummyHead.next;
-            dummyHead.next = node;
+        for (int i = 1; i < m; i++) prev = prev = prev.next;
+        ListNode subHead = prev.next;
+
+        for (int i = 0; i < n - m; i++) {
+            ListNode node = subHead.next;
+            subHead.next = subHead.next.next;
+
+            node.next = prev.next;
+            prev.next = node;
         }
 
-        return dummyHead.next;
+        head = dummyHead.next;
+        dummyHead.next = null;
+        return head;
     }
 }
