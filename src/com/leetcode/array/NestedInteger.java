0c86c09e79b4f6812ca8a2f5314e9571af6b35c1
Bofeng Li <bofeng.li@outlook.com>
2019-01-22 23:59:00 +0800

    341. Flatten Nested List Iterator

---
+++ src/com/leetcode/array/NestedInteger.java
@@ -0,0 +1,11 @@
+package com.leetcode.array;
+
+import java.util.List;
+
+public interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<NestedInteger> getList();
+}
---
+++ src/com/leetcode/array/No341FlattenNestedListIterator.java
@@ -0,0 +1,35 @@
+package com.leetcode.array;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No341FlattenNestedListIterator implements Iterator<Integer> {
+
+    private Deque<NestedInteger> dq = new LinkedList<>();
+
+    public No341FlattenNestedListIterator(List<NestedInteger> nestedList) {
+        for (NestedInteger ni : nestedList) dq.offer(ni);
+    }
+
+    private void flatten() {
+        while (!dq.isEmpty() && !dq.peek().isInteger()) {
+            List<NestedInteger> list = dq.poll().getList();
+            for (int i = list.size() - 1; i >= 0; i--) dq.addFirst(list.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) return null;
+
+        return dq.poll().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        flatten();
+        return !dq.isEmpty();
+    }
+}
