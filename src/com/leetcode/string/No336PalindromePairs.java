1a5d709cc056894714f2e6af41278e9ff72293f2
Bofeng Li <bofeng.li@outlook.com>
2019-01-22 20:41:25 +0800

    336. Palindrome Pairs

--- src/com/leetcode/string/No336PalindromePairs.java
+++ src/com/leetcode/string/No336PalindromePairs.java
@@ -8,90 +8,82 @@ public class No336PalindromePairs {
     class TrieNode {
         int wordIndex = -1;
         TrieNode[] children = new TrieNode[26];
-        List<Integer> leftPalindromeWordIds;
-
-        TrieNode() {
-            leftPalindromeWordIds = new ArrayList<>();
-        }
+        List<Integer> leftPalindromeIds = new ArrayList<>();
     }
 
-    TrieNode root = new TrieNode();
+    private TrieNode root = new TrieNode();
 
     public List<List<Integer>> palindromePairs(String[] words) {
-        List<Integer> selfPalindromeIds = getSelfPalindrome(words);
+        final List<List<Integer>> result = new ArrayList<>();
+        final List<Integer> slefPalindromeIds = findSelfPalindromeIds(words);
 
-        List<List<Integer>> R = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
             if (words[i].isEmpty()) {
-                for (int id : selfPalindromeIds) {
+                for (int id : slefPalindromeIds) {
                     if (id == i) continue;
-                    R.add(Arrays.asList(i, id));
-                    R.add(Arrays.asList(id, i));
+                    result.add(Arrays.asList(i, id));
+                    result.add(Arrays.asList(id, i));
                 }
             } else insert(words[i], i);
         }
 
         for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (word.isEmpty()) continue;
-            for (int id : search(word, i)) if (i != id) R.add(Arrays.asList(i, id));
+            if (words[i].isEmpty()) continue;
+            for (int id : search(words[i])) if (i != id) result.add(Arrays.asList(i, id));
         }
 
-        return R;
+        return result;
     }
 
+    private void insert(String w, int idx) {
+        String sw = reverse(w);
 
-    private void insert(String word, int i) {
-        word = reverse(word);
         TrieNode current = root;
-        for (int j = 0; j < word.length(); j++) {
-            char c = word.charAt(j);
+        for (int i = 0; i < sw.length(); i++) {
+            char c = sw.charAt(i);
             if (current.children[c - 'a'] == null) current.children[c - 'a'] = new TrieNode();
             current = current.children[c - 'a'];
-            if (isPalindrome(word, j + 1, word.length() - 1)) current.leftPalindromeWordIds.add(i);
+            if (isPalindrome(sw, i + 1, sw.length() - 1)) current.leftPalindromeIds.add(idx);
         }
 
-        current.wordIndex = i;
+        current.wordIndex = idx;
     }
 
-    private String reverse(String word) {
-        StringBuilder sb = new StringBuilder(word);
-        return sb.reverse().toString();
-    }
-
-    private List<Integer> search(String word, int i) {
+    private List<Integer> search(String w) {
         List<Integer> ids = new ArrayList<>();
         TrieNode current = root;
-        for (int j = 0; j < word.length(); j++) {
-            char c = word.charAt(j);
+        for (int i = 0; i < w.length(); i++) {
+            char c = w.charAt(i);
             current = current.children[c - 'a'];
             if (current == null) return ids;
-            if (current.wordIndex >= 0 && isPalindrome(word, j + 1, word.length() - 1)) {
-                ids.add(current.wordIndex);
-            }
+
+            if (current.wordIndex >= 0 && isPalindrome(w, i + 1, w.length() - 1)) ids.add(current.wordIndex);
         }
+
         if (current.wordIndex >= 0) ids.add(current.wordIndex);
-        ids.addAll(current.leftPalindromeWordIds);
+        ids.addAll(current.leftPalindromeIds);
 
         return ids;
     }
 
-    private boolean isPalindrome(String word) {
-        return isPalindrome(word, 0, word.length() - 1);
+    private List<Integer> findSelfPalindromeIds(String[] words) {
+        final List<Integer> ids = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) if (isPalindrome(words[i])) ids.add(i);
+        return ids;
     }
 
-    private boolean isPalindrome(String word, int left, int right) {
-        if (left > right) return false;
-        while (left < right) if (word.charAt(left++) != word.charAt(right--)) return false;
+    private boolean isPalindrome(String s) {
+        return isPalindrome(s, 0, s.length() - 1);
+    }
+
+    private String reverse(String s) {
+        final StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+
+    private boolean isPalindrome(String s, int lo, int hi) {
+        if (lo > hi) return false;
+        while (lo < hi) if (s.charAt(lo++) != s.charAt(hi--)) return false;
         return true;
-    }
-
-    private List<Integer> getSelfPalindrome(String[] words) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < words.length; i++) {
-            if (isPalindrome(words[i])) list.add(i);
-        }
-
-        return list;
     }
 }
