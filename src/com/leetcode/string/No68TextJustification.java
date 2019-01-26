3ef86072508f0851068fb3ead38c90b305c9fab1
Bofeng Li <bofeng.li@outlook.com>
2019-01-26 23:39:45 +0800

    68. Text Justification

--- src/com/leetcode/string/No68TextJustification.java
+++ src/com/leetcode/string/No68TextJustification.java
@@ -5,54 +5,52 @@ import java.util.List;
 
 public class No68TextJustification {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> R = new ArrayList<>();
+        List<String> result = new ArrayList<>(), list = new ArrayList<>();
 
-        int totalWordLen = 0;
-        List<String> lineWords = new ArrayList<>();
-        for (String w : words) {
-            if (totalWordLen + lineWords.size() + w.length() > maxWidth) {
-                R.add(fillLine(lineWords, totalWordLen, maxWidth));
-
-                totalWordLen = 0;
-                lineWords = new ArrayList<>();
+        int totalCharacters = 0;
+        for (String word : words) {
+            if (totalCharacters + word.length() + list.size() > maxWidth) {
+                result.add(justifyLine(list, totalCharacters, maxWidth));
+                list = new ArrayList<>();
+                list.add(word);
+                totalCharacters = word.length();
+            } else {
+                list.add(word);
+                totalCharacters += word.length();
             }
-
-            totalWordLen += w.length();
-            lineWords.add(w);
         }
-        if (!lineWords.isEmpty()) R.add(fillLastLine(lineWords, maxWidth));
+        result.add(justifyLastLine(list, maxWidth));
 
-        return R;
+        return result;
     }
 
-    private String fillLine(List<String> words, int totalWordLen, int maxWidth) {
-        int slots = words.size() - 1, extraSpacers = maxWidth - totalWordLen - slots;
-        if (slots == 0) return fillLastLine(words, maxWidth);
+    private String justifyLine(List<String> words, int totalCharacters, int maxWidth) {
+        if (words.size() == 1) return justifyLastLine(words, maxWidth);
 
-        int extraSlotSpacers = extraSpacers / slots;
-        int otherSpacers = extraSpacers - extraSlotSpacers * slots;
+        int slots = words.size() - 1, totalSpaces = maxWidth - totalCharacters;
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < words.size(); i++) {
-            sb.append(words.get(i));
-            if (i != words.size() - 1) {
-                sb.append(" ");
-                for (int k = 0; k < extraSlotSpacers; k++) sb.append(" ");
-                if (otherSpacers-- > 0) sb.append(" ");
-            }
+        for (String w : words) {
+            sb.append(w);
+            if (slots == 0) break;
+
+            int slotsSpaces = totalSpaces / slots + (totalSpaces % slots == 0 ? 0 : 1);
+            for (int i = 0; i < Math.min(slotsSpaces, totalSpaces); i++) sb.append(" ");
+
+            slots--;
+            totalSpaces -= slotsSpaces;
         }
 
         return sb.toString();
     }
 
 
-    private String fillLastLine(List<String> words, int maxWidth) {
+    private String justifyLastLine(List<String> words, int maxWidth) {
         StringBuilder sb = new StringBuilder();
-        for (String word : words) sb.append(word).append(" ");
-        sb.deleteCharAt(sb.length() - 1);
+        for (String w : words) sb.append(w).append(" ");
 
-        int spacers = maxWidth - sb.length();
-        while (spacers-- > 0) sb.append(" ");
+        while (sb.length() < maxWidth) sb.append(" ");
+        if (sb.length() > maxWidth) sb.deleteCharAt(sb.length() - 1);
 
         return sb.toString();
     }
