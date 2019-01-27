d22078032b2f1f7ea0257fcc2b4d6fee5661436f
Bofeng Li <bofeng.li@outlook.com>
2019-01-27 22:47:13 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ src/com/leetcode/array/No448FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,18 @@
+package com.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No448FindAllNumbersDisappearedInAnArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) nums[index] = -nums[index];
+        }
+
+        for (int i = 0; i < nums.length; i++) if (nums[i] > 0) list.add(i + 1);
+
+        return list;
+    }
+}
