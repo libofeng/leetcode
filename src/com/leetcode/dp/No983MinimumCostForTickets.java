a7740ae943a281a5507253bb16e666983fad8d95
Bofeng Li <bofeng.li@outlook.com>
2019-01-27 13:14:46 +0800

    983. Minimum Cost For Tickets

--- src/com/leetcode/dp/No983MinimumCostForTickets.java
+++ src/com/leetcode/dp/No983MinimumCostForTickets.java
@@ -1,25 +1,17 @@
 package com.leetcode.dp;
 
-import java.util.Arrays;
-
 public class No983MinimumCostForTickets {
     public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE / 2);
-
-        dp[0] = 0;
+        final int n = days.length;
+        final int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             dp[i] = dp[i - 1] + costs[0];
             for (int j = i - 1; j >= 0; j--) {
-                if (days[i - 1] - days[j] + 1 <= 7) {
-                    dp[i] = Math.min(dp[i], dp[j] + costs[1]);
-                }
-                if (days[i - 1] - days[j] + 1 <= 30) {
-                    dp[i] = Math.min(dp[i], dp[j] + costs[2]);
-                }
+                if (days[i - 1] - days[j] + 1 <= 7) dp[i] = Math.min(dp[i], dp[j] + costs[1]);
+                if (days[i - 1] - days[j] + 1 <= 30) dp[i] = Math.min(dp[i], dp[j] + costs[2]);
             }
         }
+
         return dp[n];
     }
 }
