5bba1397714c36e47affbf1a4a133366ec067449
Bofeng Li <bofeng.li@outlook.com>
2019-02-11 14:04:31 +0800

    1. Two Sum

--- src/com/leetcode/array/No1TwoSum.java
+++ src/com/leetcode/array/No1TwoSum.java
@@ -5,33 +5,22 @@ import java.util.Map;
 
 public class No1TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] R = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    R[0] = i;
-                    R[1] = j;
-                    break;
-                }
+                if (nums[i] + nums[j] == target) return new int[]{i, j};
             }
         }
 
-        return R;
+        return new int[2];
     }
 
     public int[] twoSum2(int[] nums, int target) {
-        int[] R = new int[2];
         final Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) map.put(nums[i], i);
-
         for (int i = 0; i < nums.length; i++) {
-            int num = target - nums[i];
-            if (map.containsKey(num) && map.get(num) != i) {
-                R[0] = i;
-                R[1] = map.get(num);
-            }
+            if (map.containsKey(target - nums[i])) return new int[]{map.get(target - nums[i]), i};
+            map.put(nums[i], i);
         }
 
-        return R;
+        return new int[2];
     }
 }
