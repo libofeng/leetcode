9c9e74ed3bf96a96e958f8f2b18aeac365b9fc4f
Bofeng Li <bofeng.li@outlook.com>
2019-02-11 23:45:05 +0800

    53. Maximum Subarray

--- src/com/leetcode/array/No53MaximumSubarray.java
+++ src/com/leetcode/array/No53MaximumSubarray.java
@@ -3,10 +3,8 @@ package com.leetcode.array;
 public class No53MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int sum = 0, maxSum = Integer.MIN_VALUE;
-        for (int n : nums) {
-            if (sum <= 0) sum = n;
-            else sum += n;
-
+        for(int n : nums){
+            sum = n + Math.max(sum, 0);
             maxSum = Math.max(maxSum, sum);
         }
 
