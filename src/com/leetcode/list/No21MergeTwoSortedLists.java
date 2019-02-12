e2ae758a1e1b48d204d92a54475b1e53399a236e
Bofeng Li <bofeng.li@outlook.com>
2019-02-12 12:23:26 +0800

    21. Merge Two Sorted Lists

--- src/com/leetcode/list/No21MergeTwoSortedLists.java
+++ src/com/leetcode/list/No21MergeTwoSortedLists.java
@@ -8,4 +8,30 @@ public class No21MergeTwoSortedLists {
         head.next = mergeTwoLists(head.next, head == l1 ? l2 : l1);
         return head;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return l1 == null ? l2 : l1;
+
+        ListNode dummy = new ListNode(0), p = dummy;
+        while (l1 != null && l2 != null) {
+            ListNode node;
+            if (l1.val < l2.val) {
+                node = l1;
+                l1 = l1.next;
+            } else {
+                node = l2;
+                l2 = l2.next;
+            }
+
+            p.next = node;
+            p = p.next;
+        }
+
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
+
+        ListNode head = dummy.next;
+        dummy.next = null;
+        return head;
+    }
 }
