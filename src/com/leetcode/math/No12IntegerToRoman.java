e69746728dc288d9533444544368e734c35cbe1c
Bofeng Li <bofeng.li@outlook.com>
2019-02-12 18:14:18 +0800

    12. Integer to Roman

--- src/com/leetcode/math/No12IntegerToRoman.java
+++ src/com/leetcode/math/No12IntegerToRoman.java
@@ -2,10 +2,11 @@ package com.leetcode.math;
 
 public class No12IntegerToRoman {
     public String intToRoman(int num) {
-        String[] M = {"", "M", "MM", "MMM"};
-        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+        final String[] M = new String[]{"", "M", "MM", "MMM"};
+        final String[] C = new String[]{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        final String[] X = new String[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        final String[] I = new String[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return M[num / 1000] + C[num % 1000 / 100] + X[num % 100 / 10] + I[num % 10];
     }
 }
