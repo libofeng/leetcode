992bb73f8ac25778882990a58802b01ace7da3ea
Bofeng Li <bofeng.li@outlook.com>
2019-02-12 17:26:09 +0800

    121. Best Time to Buy and Sell Stock

--- src/com/leetcode/array/No121BestTimeToBuyAndSellStock.java
+++ src/com/leetcode/array/No121BestTimeToBuyAndSellStock.java
@@ -2,12 +2,12 @@ package com.leetcode.array;
 
 public class No121BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0 || prices.length == 1) return 0;
+        if (prices == null || prices.length <= 1) return 0;
 
         int min = prices[0], maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-            maxProfit = Math.max(maxProfit, prices[i] - min);
             min = Math.min(min, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - min);
         }
 
         return maxProfit;
