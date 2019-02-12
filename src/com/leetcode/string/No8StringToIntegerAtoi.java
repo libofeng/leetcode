9870ef93b839b94f0442e2a0544543bd7d84bf7b
Bofeng Li <bofeng.li@outlook.com>
2019-02-12 19:23:15 +0800

    8. String to Integer (atoi)

--- src/com/leetcode/string/No8StringToIntegerAtoi.java
+++ src/com/leetcode/string/No8StringToIntegerAtoi.java
@@ -2,52 +2,22 @@ package com.leetcode.string;
 
 public class No8StringToIntegerAtoi {
     public int myAtoi(String str) {
-        if (str == null || str.length() == 0) return 0;
-        int len = str.length();
+        str = str.trim();
+        if (str.isEmpty()) return 0;
 
-        boolean minus = false;
-        //find valid start
-        int i = 0;
-        for (; i < len; i++) {
+        char first = str.charAt(0);
+        int sign = first == '-' ? -1 : 1, start = first == '-' || first == '+' ? 1 : 0;
+        long num = 0;
+        for (int i = start; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (c == ' ') {
-                continue;
-            } else if (c == '-') {
-                minus = true;
-                i++;
-                break;
-            } else if (c == '+') {
-                i++;
-                break;
-            } else if (c >= '0' && c <= '9') {
-                break;
-            } else {
-                return 0;
-            }
+            if (!Character.isDigit(c)) break;
+
+            num = num * 10 + (c - '0') * sign;
+            if (num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
         }
 
-
-        int number = 0;
-        for (int j = i; j < len; j++) {
-            char c = str.charAt(j);
-
-            if (c < '0' || c > '9') break;// invalid
-            if (number >= (Integer.MAX_VALUE - (c - '0')) / 10) {
-                if (minus) {
-                    if (number == (Integer.MAX_VALUE - (c - '0')) / 10) {
-                        return -Integer.MAX_VALUE;
-                    } else {
-                        return Integer.MIN_VALUE;
-                    }
-                } else {
-                    return Integer.MAX_VALUE;
-                }
-            } else {
-                number = number * 10 + (c - '0');
-            }
-        }
-
-        return minus ? -number : number;
+        return (int) num;
     }
 
 
