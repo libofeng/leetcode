345d0b60aec62227f24c9d57e329ed6d13b7f5d2
Bofeng Li <bofeng.li@outlook.com>
2019-02-13 12:56:39 +0800

    283. Move Zeroes

--- src/com/leetcode/array/No283MoveZeroes.java
+++ src/com/leetcode/array/No283MoveZeroes.java
@@ -14,8 +14,20 @@ public class No283MoveZeroes {
     // Minimize the total number of operations.
     public void moveZeroes2(int[] nums) {
         int index = 0;
-        while(index<nums.length && nums[index]!=0) index++;
-        for(int i = index+1; i<nums.length; i++) if(nums[i]!= 0) nums[index++] = nums[i];
-        while(index<nums.length) nums[index++] = 0;
+        while (index < nums.length && nums[index] != 0) index++;
+        for (int i = index + 1; i < nums.length; i++) if (nums[i] != 0) nums[index++] = nums[i];
+        while (index < nums.length) nums[index++] = 0;
+    }
+
+    // one pass
+    public void moveZeroes3(int[] nums) {
+        int index = 0;
+        while (index < nums.length && nums[index] != 0) index++;
+        for (int i = index + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+                nums[i] = 0;
+            }
+        }
     }
 }
