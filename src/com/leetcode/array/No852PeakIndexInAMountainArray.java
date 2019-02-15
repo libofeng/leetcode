b8ea937f86052184feb0af77c6abf332a9d4a5fe
Bofeng Li <bofeng.li@outlook.com>
2019-02-15 22:36:45 +0800

    852. Peak Index in a Mountain Array

--- src/com/leetcode/array/No852PeakIndexInAMountainArray.java
+++ src/com/leetcode/array/No852PeakIndexInAMountainArray.java
@@ -5,4 +5,17 @@ public class No852PeakIndexInAMountainArray {
         for (int i = 1; i < A.length - 1; i++) if (A[i] > A[i + 1]) return i;
         return 0;
     }
+
+    public int peakIndexInMountainArray2(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) return mid;
+
+            if (A[mid] > A[mid - 1]) lo = mid + 1;
+            else hi = mid - 1;
+        }
+
+        return lo;
+    }
 }
