fb0a1986838ee7abe7b9404cde2b1869f8860fbe
Bofeng Li <bofeng.li@outlook.com>
2019-02-15 14:27:57 +0800

    169. Majority Element

--- src/com/leetcode/array/No169MajorityElement.java
+++ src/com/leetcode/array/No169MajorityElement.java
@@ -1,6 +1,8 @@
 package com.leetcode.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class No169MajorityElement {
     public int majorityElement(int[] nums) {
@@ -8,7 +10,19 @@ public class No169MajorityElement {
         return nums[nums.length / 2];
     }
 
+
     public int majorityElement2(int[] nums) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums) {
+            int count = map.getOrDefault(n, 0);
+            if(count == nums.length / 2) return n;
+            map.put(n, count + 1);
+        }
+
+        return -1;
+    }
+
+    public int majorityElement3(int[] nums) {
         int R = 0, count = 0;
         for (int num : nums) {
             if (count == 0) {
