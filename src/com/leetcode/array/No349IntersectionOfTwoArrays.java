f59f6fc200fb02e1af8d91f768d3e06233d5d63a
Bofeng Li <bofeng.li@outlook.com>
2019-02-19 12:29:25 +0800

    349. Intersection of Two Arrays

--- src/com/leetcode/array/No349IntersectionOfTwoArrays.java
+++ src/com/leetcode/array/No349IntersectionOfTwoArrays.java
@@ -21,29 +21,21 @@ public class No349IntersectionOfTwoArrays {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; ) {
-            if (i > 0 && nums1[i] == nums1[i - 1]) {
-                i++;
-                continue;
-            }
-            if (j > 0 && nums2[j] == nums2[j - 1]) {
-                j++;
-                continue;
-            }
-
+        int i = 0, j = 0;
+        Set<Integer> set = new HashSet<>();
+        while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) i++;
             else if (nums1[i] > nums2[j]) j++;
             else {
-                list.add(nums1[i]);
+                set.add(nums1[i]);
                 i++;
                 j++;
             }
         }
 
-        int idx = 0;
-        int[] result = new int[list.size()];
-        for (int n : list) result[idx++] = n;
+        final int[] result = new int[set.size()];
+        int index = 0;
+        for (int n : set) result[index++] = n;
 
         return result;
     }
