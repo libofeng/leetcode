1f3b3ff8635554d3af9348127ec96ebb37ff4825
Bofeng Li <bofeng.li@outlook.com>
2019-02-20 13:29:09 +0800

    62. Unique Paths

--- src/com/leetcode/dfs/No62UniquePaths.java
+++ src/com/leetcode/dfs/No62UniquePaths.java
@@ -2,6 +2,11 @@ package com.leetcode.dfs;
 
 public class No62UniquePaths {
     public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1;
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+
+    public int uniquePaths2(int m, int n) {
         return dfs(m, n, new int[m + 1][n + 1]);
     }
 
@@ -13,12 +18,24 @@ public class No62UniquePaths {
         return cache[m][n] = dfs(m - 1, n, cache) + dfs(m, n - 1, cache);
     }
 
-    public int uniquePaths2(int m, int n) {
-        final int[] dp = new int[n];
-        dp[0] = 1;
+    // dp[i][j] = dp[i-1] + dp[i][j-1]
+    // dp[1][j] = 1;
+    // dp[i][1] = 1;
+    public int uniquePaths3(int m, int n) {
+        final int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) dp[i][1] = 1;
+        for (int j = 1; j <= n; j++) dp[1][j] = 1;
 
-        for (int i = 0; i < m; i++) for (int j = 1; j < n; j++) dp[j] = dp[j - 1] + dp[j];
-        return dp[n - 1];
+        for (int i = 2; i <= m; i++) for (int j = 2; j <= n; j++) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        return dp[m][n];
+    }
+
+    public int uniquePaths4(int m, int n) {
+        final int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 1; i <= m; i++) for (int j = 1; j <= n; j++) dp[j] += dp[j - 1];
+        return dp[n];
     }
 
 }
