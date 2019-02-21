7ab2fc5f80a3cc2153739ccf62ff2b14ad1de7d5
Bofeng Li <bofeng.li@outlook.com>
2019-02-21 11:19:35 +0800

    621. Task Scheduler

--- src/com/leetcode/greedy/No621TaskScheduler.java
+++ src/com/leetcode/greedy/No621TaskScheduler.java
@@ -52,6 +52,17 @@ public class No621TaskScheduler {
     }
 
     // https://leetcode.com/problems/task-scheduler/discuss/104493/c-java-clean-code-priority-queue
+/*
+    The idea is:
+            0. To work on the same task again, CPU has to wait for time n, therefore we can think of as if there is a cycle,
+            of time n+1, regardless whether you schedule some other task in the cycle or not.
+
+            1. To avoid leave the CPU with limited choice of tasks and having to sit there cooling down frequently at the end,
+            it is critical the keep the diversity of the task pool for as long as possible.
+
+            2. In order to do that, we should try to schedule the CPU to always try round robin between the most popular tasks at any time.
+*/
+
     public int leastInterval3(char[] tasks, int n) {
         final Map<Character, Integer> count = new HashMap<>();
         for (char c : tasks) count.put(c, count.getOrDefault(c, 0) + 1);
