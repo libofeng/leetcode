5de10d6945b30ba3f52274642e8698ebf39b9cb2
Bofeng Li <bofeng.li@outlook.com>
2019-02-21 11:51:23 +0800

    348. Design Tic-Tac-Toe

---
+++ src/com/leetcode/array/No348DesignTicTacToe.java
@@ -0,0 +1,45 @@
+package com.leetcode.array;
+
+public class No348DesignTicTacToe {
+    private int[][] rows;
+    private int[][] cols;
+    private int[] diag = new int[2];
+    private int[] adiag = new int[2];
+    private int size;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public No348DesignTicTacToe(int n) {
+        size = n;
+        rows = new int[n][2];
+        cols = new int[n][2];
+    }
+
+    /**
+     * Player {player} makes a move at ({row}, {col}).
+     *
+     * @param row    The row of the board.
+     * @param col    The column of the board.
+     * @param player The player, can be either 1 or 2.
+     * @return The current winning condition, can be either:
+     * 0: No one wins.
+     * 1: Player 1 wins.
+     * 2: Player 2 wins.
+     */
+    public int move(int row, int col, int player) {
+        rows[row][player - 1]++;
+        if (rows[row][player - 1] == size) return player;
+        cols[col][player - 1]++;
+        if (cols[col][player - 1] == size) return player;
+        if (row == col) {
+            diag[player - 1]++;
+            if (diag[player - 1] == size) return player;
+        }
+        if (row + col == size - 1) {
+            adiag[player - 1]++;
+            if (adiag[player - 1] == size) return player;
+        }
+        return 0;
+    }
+}
