0700b667513f4ed686baa2be39182cd47156cc65
Bofeng Li <bofeng.li@outlook.com>
2019-02-23 23:04:43 +0800

    Update dot product of 2 sparse vector.

--- src/com/facebook/DotProduct.java
+++ src/com/facebook/DotProduct.java
@@ -1,48 +1,39 @@
 package com.facebook;
 
+import java.util.Arrays;
+
 public class DotProduct {
-    public int dotPro(int[][] v1, int[][] v2) {
-        int[][] shortV;
-        int[][] longV;
-        if (v1.length < v2.length) {
-            shortV = v1;
-            longV = v2;
-        } else {
-            shortV = v2;
-            longV = v1;
-        }
+    public int dotPro(int[][] vectorA, int[][] vectorB) {
+        if (vectorA.length > vectorB.length) return dotPro(vectorB, vectorA);
+        Arrays.sort(vectorB, (a, b) -> a[0] - a[1]);
 
         int res = 0;
-        for (int i = 0; i < shortV.length; i++) {
-            int shortIndex = shortV[i][0];
-            int shortValue = shortV[i][1];
-            int longSeq = binarySearch(longV, shortIndex);
-            if (longSeq >= 0) {
-                res += shortValue * longV[longSeq][1];
-            }
+        for (int[] v : vectorA) {
+            int index = v[0], n1 = v[1];
+
+            int idx = binarySearch(vectorB, index);
+            if (idx >= 0) res += n1 * vectorB[idx][1];
         }
         return res;
     }
 
-    public int binarySearch(int[][] arr, int target) {
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (arr[m][0] == target) return m;
-            else if (arr[m][0] < target) l = m + 1;
-            else r = m - 1;
+    private int binarySearch(int[][] vector, int target) {
+        int lo = 0, hi = vector.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (vector[mid][0] == target) return mid;
+
+            if (target < vector[mid][0]) hi = mid - 1;
+            else lo = mid + 1;
         }
+
         return -1;
     }
 
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         DotProduct sol = new DotProduct();
-        int[][] v2 = new int[][]{{0, 2}, {1, 3}, {5, 2}, {7, 1}, {10, 1}};
+        int[][] v2 = new int[][]{{1, 3}, {0, 2}, {5, 2}, {7, 1}, {10, 1}};
         int[][] v1 = new int[][]{{1, 6}, {7, 2}};
         int res = sol.dotPro(v1, v2);
         System.out.println(res);
