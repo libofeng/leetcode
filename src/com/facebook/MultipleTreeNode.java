04598cdc1cc3a22b98a4b63fe11761ef4070aacf
Bofeng Li <bofeng.li@outlook.com>
2019-02-25 11:39:28 +0800

    Update class name.

---
+++ src/com/facebook/MultipleTreeNode.java
@@ -0,0 +1,13 @@
+package com.facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleTreeNode {
+    int val;
+    List<MultipleTreeNode> children = new ArrayList<>();
+
+    public MultipleTreeNode(int val) {
+        this.val = val;
+    }
+}
--- src/com/facebook/SmallestSubtreeWithMostDepth.java
+++ src/com/facebook/SmallestSubtreeWithMostDepth.java
@@ -5,16 +5,16 @@ import java.util.Map;
 
 public class SmallestSubtreeWithMostDepth {
 
-    MutilTreeNode find(MutilTreeNode root) {
+    MultipleTreeNode find(MultipleTreeNode root) {
         return helper(root, maxDepth(root));
     }
 
-    private MutilTreeNode helper(MutilTreeNode root, int depth) {
+    private MultipleTreeNode helper(MultipleTreeNode root, int depth) {
         if (root == null || root.children.isEmpty()) return root;
 
         int count = 0;
-        MutilTreeNode next = null;
-        for (MutilTreeNode N : root.children) {
+        MultipleTreeNode next = null;
+        for (MultipleTreeNode N : root.children) {
             if (depthMap.get(N) == depth - 1) {
                 count++;
                 next = N;
@@ -23,45 +23,45 @@ public class SmallestSubtreeWithMostDepth {
         return count > 1 ? root : helper(next, depth - 1);
     }
 
-    Map<MutilTreeNode, Integer> depthMap = new HashMap<>();
+    Map<MultipleTreeNode, Integer> depthMap = new HashMap<>();
 
-    private int maxDepth(MutilTreeNode root) {
+    private int maxDepth(MultipleTreeNode root) {
         if (root == null) return 0;
 
         int max = 0;
-        for (MutilTreeNode N : root.children) max = Math.max(max, maxDepth(N));
+        for (MultipleTreeNode N : root.children) max = Math.max(max, maxDepth(N));
 
         depthMap.put(root, max + 1);
         return max + 1;
     }
 
     public static void main(String[] args) {
-        MutilTreeNode root = new MutilTreeNode(5);
-        MutilTreeNode child1 = new MutilTreeNode(3);
-        MutilTreeNode child2 = new MutilTreeNode(4);
-        MutilTreeNode child3 = new MutilTreeNode(6);
+        MultipleTreeNode root = new MultipleTreeNode(5);
+        MultipleTreeNode child1 = new MultipleTreeNode(3);
+        MultipleTreeNode child2 = new MultipleTreeNode(4);
+        MultipleTreeNode child3 = new MultipleTreeNode(6);
         root.children.add(child1);
         root.children.add(child2);
         root.children.add(child3);
 
-        MutilTreeNode child11 = new MutilTreeNode(2);
-        child11.children.add(new MutilTreeNode(10));
+        MultipleTreeNode child11 = new MultipleTreeNode(2);
+        child11.children.add(new MultipleTreeNode(10));
         child1.children.add(child11);
 
-        MutilTreeNode child21 = new MutilTreeNode(1);
-        MutilTreeNode child211 = new MutilTreeNode(11);
+        MultipleTreeNode child21 = new MultipleTreeNode(1);
+        MultipleTreeNode child211 = new MultipleTreeNode(11);
         child21.children.add(child211);
-        MutilTreeNode child212 = new MutilTreeNode(21);
+        MultipleTreeNode child212 = new MultipleTreeNode(21);
         child211.children.add(child212);
-        MutilTreeNode child213 = new MutilTreeNode(31);
+        MultipleTreeNode child213 = new MultipleTreeNode(31);
         child212.children.add(child213);
         child1.children.add(child21);
 
-        child3.children.add(new MutilTreeNode(8));
-        child3.children.add(new MutilTreeNode(9));
+        child3.children.add(new MultipleTreeNode(8));
+        child3.children.add(new MultipleTreeNode(9));
 
         SmallestSubtreeWithMostDepth solution = new SmallestSubtreeWithMostDepth();
-        MutilTreeNode subRoot = solution.find(root);
+        MultipleTreeNode subRoot = solution.find(root);
 
         System.out.println("subRoot.val = " + subRoot.val);
 
@@ -70,7 +70,7 @@ public class SmallestSubtreeWithMostDepth {
         System.out.println("subRoot = " + subRoot);
 
         solution = new SmallestSubtreeWithMostDepth();
-        subRoot = solution.find(new MutilTreeNode(1));
+        subRoot = solution.find(new MultipleTreeNode(1));
         System.out.println("subRoot.val = " + subRoot.val);
 
     }
