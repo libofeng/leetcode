fcf49826acfea421073bb853178ecc1a75da0dca
Bofeng Li <bofeng.li@outlook.com>
2019-02-25 22:54:52 +0800

    110. Balanced Binary Tree

--- src/com/leetcode/tree/No110BalancedBinaryTree.java
+++ src/com/leetcode/tree/No110BalancedBinaryTree.java
@@ -3,18 +3,14 @@ package com.leetcode.tree;
 public class No110BalancedBinaryTree {
 
     public boolean isBalanced(TreeNode root) {
-        return height(root) > -1;
+        return height(root) != -1;
     }
 
     private int height(TreeNode root) {
         if (root == null) return 0;
 
-        int lh = height(root.left);
-        if (lh < 0) return -1;
-        int rh = height(root.right);
-        if (rh < 0) return -1;
-
-        if (Math.abs(lh - rh) > 1) return -1;
-        return Math.max(lh, rh) + 1;
+        int left = height(root.left), right = height(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        return Math.max(left, right) + 1;
     }
 }
