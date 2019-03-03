c7902092700deaa4191a236810f96ca1c0a051dc
Bofeng Li <bofeng.li@outlook.com>
2019-03-03 12:18:28 +0800

    1000. Minimum Cost to Merge Stones

--- src/com/leetcode/contest/contest126/No1000MinimumCostToMergeStones.java
+++ src/com/leetcode/contest/contest126/No1000MinimumCostToMergeStones.java
@@ -1,38 +1,40 @@
 package com.leetcode.contest.contest126;
 
 public class No1000MinimumCostToMergeStones {
-    // this is NOT right solution
+    public int mergeStones(int[] a, int K) {
+        int n = a.length;
+        if (n % (K - 1) != 1 % (K - 1)) {
+            return -1;
+        }
+        int[] cum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            cum[i + 1] = cum[i] + a[i];
+        }
 
-    public int mergeStones(int[] stones, int K) {
-        if (stones.length <= 1) return 0;
-        if (stones.length < K) return -1;
-
-        int minSum = Integer.MAX_VALUE, minStart = -1, start = -1, sum = 0;
-        for (int i = 0; i < stones.length; i++) {
-            sum += stones[i];
-            if (i - start == K) {
-                if (sum <= minSum) {
-                    minSum = sum;
-                    minStart = ++start;
+        int[][] dp = new int[n][n];
+        for (int len = K; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                int cost = Integer.MAX_VALUE;
+                int ulen = (len - 1) / (K - 1) * (K - 1) + 1;
+                if (ulen % (K - 1) == 1 % (K - 1)) {
+                    ulen -= K - 1;
                 }
-
-                sum -= stones[start];
+                int s = len % (K - 1) == 1 % (K - 1) ? len - (K - 1) : len;
+                for (int k = i + 1; k <= j; k++) {
+                    if ((k - i - 1) / (K - 1) + (j - k + 1 - 1) / (K - 1) == (s - 1) / (K - 1)) {
+                        int c = 0;
+                        if (i <= k - 1) c += dp[i][k - 1];
+                        if (k <= j) c += dp[k][j];
+                        cost = Math.min(cost, c);
+                    }
+                }
+                if (len % (K - 1) == 1 % (K - 1)) {
+                    cost += cum[j + 1] - cum[i];
+                }
+                dp[i][j] = cost;
             }
         }
-
-        int[] list = new int[stones.length - K + 1];
-        for (int i = 0, j = 0; i < list.length; i++) {
-            if (i == minStart) {
-                list[i] = minSum;
-                j += K;
-            } else {
-                list[i] = stones[j];
-
-                j++;
-            }
-        }
-
-        int next = mergeStones(list, K);
-        return next == -1 ? -1 : minSum + next;
+        return dp[0][n - 1];
     }
 }
