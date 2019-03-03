886f3fe93973f9e3f8b959b33368fee2c42d44e0
Bofeng Li <bofeng.li@outlook.com>
2019-03-03 12:19:52 +0800

    1000. Minimum Cost to Merge Stones

--- src/com/leetcode/contest/contest126/No1000MinimumCostToMergeStones.java
+++ src/com/leetcode/contest/contest126/No1000MinimumCostToMergeStones.java
@@ -37,4 +37,42 @@ public class No1000MinimumCostToMergeStones {
         }
         return dp[0][n - 1];
     }
+
+    public int mergeStones2(int[] stones, int K) {
+        int n = stones.length;
+        int[] pre = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + stones[i - 1];
+        }
+        int[][][] dp = new int[205][205][205];
+        int INF = 1000000000;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    dp[i][j][k] = INF;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][i][1] = 0;
+        }
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i + len - 1 <= n; i++) {
+                int j = i + len - 1;
+                for (int k = 2; k <= len; k++) {
+                    for (int t = i; t + 1 <= j; t++) {
+                        dp[i][j][k] = Math.min(dp[i][j][k], dp[i][t][k - 1] + dp[t + 1][j][1]);
+                    }
+                }
+
+                dp[i][j][1] = Math.min(dp[i][j][1], dp[i][j][K] + pre[j] - pre[i - 1]);
+            }
+        }
+
+
+        if (dp[1][n][1] >= INF) {
+            return -1;
+        }
+        return dp[1][n][1];
+    }
 }
