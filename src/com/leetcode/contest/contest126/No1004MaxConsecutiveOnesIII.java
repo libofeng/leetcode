8ba48094e3c65dba169f0e2b8d859c9cb5563b4d
Bofeng Li <bofeng.li@outlook.com>
2019-03-03 12:20:29 +0800

    1004. Max Consecutive Ones III

--- src/com/leetcode/contest/contest126/No1004MaxConsecutiveOnesIII.java
+++ src/com/leetcode/contest/contest126/No1004MaxConsecutiveOnesIII.java
@@ -2,7 +2,14 @@ package com.leetcode.contest.contest126;
 
 public class No1004MaxConsecutiveOnesIII {
     public int longestOnes(int[] A, int K) {
-        // TBD
-        return 0;
+        int res = 0, zero = 0, left = 0, k = K;
+        for (int right = 0; right < A.length; ++right) {
+            if (A[right] == 0) ++zero;
+            while (zero > k) {
+                if (A[left++] == 0) --zero;
+            }
+            res = Math.max(res, right - left + 1);
+        }
+        return res;
     }
 }
