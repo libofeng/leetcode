8435c248af70d6bfb1daefd6ef3f636cfba9c5b1
Bofeng Li <bofeng.li@outlook.com>
2019-03-06 12:10:37 +0800

    468. Validate IP Address

--- src/com/leetcode/string/No468ValidateIpAddress.java
+++ src/com/leetcode/string/No468ValidateIpAddress.java
@@ -5,42 +5,49 @@ public class No468ValidateIpAddress {
         final String INVALID = "Neither", IPv4 = "IPv4", IPv6 = "IPv6";
         if (IP == null || IP.isEmpty()) return INVALID;
 
-        int dotIndex = IP.indexOf("."), colonIndex = IP.indexOf(":");
-        if ((dotIndex > 0 && colonIndex > 0) || (dotIndex <= 0 && colonIndex <= 0)) return INVALID;
+        if (IP.indexOf(".") > 0 && isValidIPv4(IP)) return IPv4;
+        if (IP.indexOf(":") > 0 && isValidIPv6(IP)) return IPv6;
 
-        if (dotIndex > 0) {
-            if (IP.endsWith(".")) return INVALID;
-            String[] sections = IP.split("\\.");
-            if (sections.length != 4) return INVALID;
-            for (String section : sections) {
-                if (section.isEmpty() || section.length() > 3
-                        || (section.length() > 1 && section.startsWith("0"))) return INVALID;
+        return INVALID;
+    }
 
-                int n = 0;
-                for (int i = 0; i < section.length(); i++) {
-                    char c = section.charAt(i);
-                    if (!Character.isDigit(c)) return INVALID;
-                    n = n * 10 + (c - '0');
-                }
+    private boolean isValidIPv4(String IP) {
+        if (IP.startsWith(".") || IP.endsWith(".")) return false;
 
-                if (n > 255) return INVALID;
+        String[] sections = IP.split("\\.");
+        if (sections.length != 4) return false;
+        for (String section : sections) {
+            if (section.isEmpty() || section.length() > 3
+                    || (section.length() > 1 && section.startsWith("0"))) return false;
+
+            int n = 0;
+            for (int i = 0; i < section.length(); i++) {
+                char c = section.charAt(i);
+                if (!Character.isDigit(c)) return false;
+                n = n * 10 + (c - '0');
             }
 
-            return IPv4;
-        } else {
-            if (IP.endsWith(":")) return INVALID;
-            String[] sections = IP.split(":");
-            if (sections.length != 8) return INVALID;
-            for (String section : sections) {
-                if (section.isEmpty() || section.length() > 4) return INVALID;
-                for (int i = 0; i < section.length(); i++) {
-                    char c = section.charAt(i);
-                    if (!Character.isLetterOrDigit(c)) return INVALID;
-                    if (Character.isLetter(c) && Character.toLowerCase(c) > 'f') return INVALID;
-                }
+            if (n > 255) return false;
+        }
+
+        return true;
+    }
+
+    private boolean isValidIPv6(String IP) {
+        if (IP.startsWith(":") || IP.endsWith(":")) return false;
+
+        String[] sections = IP.split(":");
+        if (sections.length != 8) return false;
+
+        for (String section : sections) {
+            if (section.isEmpty() || section.length() > 4) return false;
+            for (int i = 0; i < section.length(); i++) {
+                char c = section.charAt(i);
+                if (!Character.isLetterOrDigit(c)) return false;
+                if (Character.isLetter(c) && Character.toLowerCase(c) > 'f') return false;
             }
         }
 
-        return IPv6;
+        return true;
     }
 }
