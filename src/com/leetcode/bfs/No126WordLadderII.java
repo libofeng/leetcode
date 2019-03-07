2ae67dfa9332694ae6cc361708d2df09ada12f32
Bofeng Li <bofeng.li@outlook.com>
2019-03-07 17:59:22 +0800

    126. Word Ladder II

--- src/com/leetcode/bfs/No126WordLadderII.java
+++ src/com/leetcode/bfs/No126WordLadderII.java
@@ -3,68 +3,50 @@ package com.leetcode.bfs;
 import java.util.*;
 
 public class No126WordLadderII {
-    class Node {
-        String s;
-        Node prev;
-
-        Node(String s, Node prev) {
-            this.s = s;
-            this.prev = prev;
-        }
-    }
-
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        final List<List<String>> R = new LinkedList<>();
+        final List<List<String>> result = new ArrayList<>();
         final Set<String> dict = new HashSet<>(wordList);
-        if (!dict.contains(endWord)) return R;
+        if (!dict.contains(endWord)) return result;
 
-        final Queue<Node> q = new LinkedList<>();
-        final List<Node> matched = new LinkedList<>();
-        q.offer(new Node(beginWord, null));
-        boolean found = false;
+        final Queue<List<String>> q = new LinkedList<>();
+        List<String> startPath = new ArrayList<>();
+        startPath.add(beginWord);
+        q.offer(startPath);
 
         while (!q.isEmpty()) {
-            final int size = q.size();
-            final Set<String> used = new HashSet<>();
-
-            for (int i = 0; i < size; i++) {
-                Node node = q.poll();
-                if (endWord.equals(node.s)) {
-                    matched.add(node);
-                    found = true;
+            int size = q.size();
+            Set<String> used = new HashSet<>();
+            while (size-- > 0) {
+                List<String> path = q.poll();
+                String lastWord = path.get(path.size() - 1);
+                if (lastWord.equals(endWord)) {
+                    result.add(path);
+                    continue;
                 }
-                if (found) continue;
 
-                char[] wordChars = node.s.toCharArray();
-                for (int j = 0; j < wordChars.length; j++) {
-                    char oldC = wordChars[j];
-
+                char[] chars = lastWord.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    char originC = chars[i];
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == oldC) continue;
+                        if (originC == c) continue;
+                        chars[i] = c;
 
-                        wordChars[j] = c;
-                        String nextWord = new String(wordChars);
-                        if (dict.contains(nextWord)) {
-                            used.add(nextWord);
-                            q.offer(new Node(nextWord, node));
+                        String next = new String(chars);
+                        if (dict.contains(next)) {
+                            used.add(next);
+                            List<String> newPath = new ArrayList<>(path);
+                            newPath.add(next);
+                            q.offer(newPath);
                         }
                     }
-
-                    wordChars[j] = oldC;
+                    chars[i] = originC;
                 }
             }
 
             dict.removeAll(used);
+            if (result.size() > 0) return result;
         }
 
-        for (Node node : matched) {
-            LinkedList<String> list = new LinkedList<>();
-            while (node != null) {
-                list.addFirst(node.s);
-                node = node.prev;
-            }
-            R.add(list);
-        }
-        return R;
+        return result;
     }
 }
