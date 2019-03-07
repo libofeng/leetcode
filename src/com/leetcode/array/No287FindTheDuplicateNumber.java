cdb110e692306bea9a4cda6a8f3efe35721b573e
Bofeng Li <bofeng.li@outlook.com>
2019-03-07 12:03:19 +0800

    287. Find the Duplicate Number

--- src/com/leetcode/array/No287FindTheDuplicateNumber.java
+++ src/com/leetcode/array/No287FindTheDuplicateNumber.java
@@ -60,4 +60,15 @@ public class No287FindTheDuplicateNumber {
         }
         return slow;
     }
+
+
+    public int findDuplicate6(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int n = Math.abs(nums[i]);
+            if (nums[n] < 0) return n;
+            else nums[n] = -nums[n];
+        }
+
+        return -1;
+    }
 }
