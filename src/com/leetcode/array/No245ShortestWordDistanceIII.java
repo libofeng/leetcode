bab7a037e516e71519bd533c65910b3c31c0b727
Bofeng Li <bofeng.li@outlook.com>
2019-03-17 14:39:48 +0800

    Update Shortest word distance III with 2 solutions.

--- src/com/leetcode/array/No245ShortestWordDistanceIII.java
+++ src/com/leetcode/array/No245ShortestWordDistanceIII.java
@@ -3,14 +3,30 @@ package com.leetcode.array;
 public class No245ShortestWordDistanceIII {
     // https://www.cnblogs.com/lightwindy/p/9736293.html
     public int shortestDistance(String[] words, String word1, String word2) {
-        boolean sameWord = word1.equals(word2);
-
+        boolean isSameWord = word1.equals(word2);
         int d = Integer.MAX_VALUE, m = -1, n = -1;
         for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word1)) m = sameWord? n : i;
+            if (!word1.equals(words[i]) && !word2.equals(words[i])) continue;
+
+            if (words[i].equals(word1)) m = isSameWord ? n : i;
             if (words[i].equals(word2)) n = i;
 
             if (m >= 0 && n >= 0) d = Math.min(d, Math.abs(m - n));
+        }
+        return d;
+    }
+
+    public int shortestDistance2(String[] words, String word1, String word2) {
+        boolean isSameWord = word1.equals(word2);
+        int d = Integer.MAX_VALUE, prev = -1;
+        for (int i = 0; i < words.length; i++) {
+            if (!word1.equals(words[i]) && !word2.equals(words[i])) continue;
+
+            if (prev != -1 && (isSameWord || !words[prev].equals(words[i]))) {
+                d = Math.min(d, Math.abs(prev - i));
+            }
+
+            prev = i;
         }
         return d;
     }
