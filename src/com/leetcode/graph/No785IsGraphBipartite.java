d3ab30782eb41e4d3887a71672b602415ba83af9
Bofeng Li <bofeng.li@outlook.com>
2019-03-17 16:15:53 +0800

    Update 785. Is Graph Bipartite?

--- src/com/leetcode/graph/No785IsGraphBipartite.java
+++ src/com/leetcode/graph/No785IsGraphBipartite.java
@@ -5,26 +5,41 @@ import java.util.Queue;
 
 public class No785IsGraphBipartite {
     public boolean isBipartite(int[][] graph) {
-        final int nodes = graph.length;
-        final int[] colors = new int[nodes];
-        final Queue<Integer> q = new LinkedList<>();
+        final int n = graph.length;
+        final int[] colors = new int[n];
 
-        for (int i = 0; i < nodes; i++) {
+        for (int i = 0; i < n; i++) {
             if (colors[i] != 0) continue;
+            if (!bfs(graph, colors, i, 1)) return false;
+        }
 
-            q.offer(i);
-            colors[i] = 1;
-            while (!q.isEmpty()) {
-                int size = q.size();
+        return true;
+    }
 
-                while (size-- > 0) {
-                    int n = q.poll(), nextColor = -colors[n];
-                    for (int next : graph[n]) {
-                        if (colors[next] == 0) {
-                            colors[next] = nextColor;
-                            q.offer(next);
-                        } else if (colors[next] != nextColor) return false;
-                    }
+    private boolean dfs(int[][] graph, int[] colors, int i, int color) {
+        if (colors[i] != 0) return colors[i] == color;
+
+        colors[i] = color;
+        for (int next : graph[i]) if (!dfs(graph, colors, next, -color)) return false;
+        return true;
+    }
+
+    private boolean bfs(int[][] graph, int[] colors, int i, int color) {
+        final Queue<Integer> q = new LinkedList<>();
+        colors[i] = color;
+        q.offer(i);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            color = -color;
+            while (size-- > 0) {
+                int n = q.poll();
+                for (int next : graph[n]) {
+                    if (colors[next] == 0) {
+                        colors[next] = color;
+                        q.offer(next);
+                    } else if (colors[next] != color) return false;
+
                 }
             }
         }
