49c93007c6c0e430ed32f8f93cdf16fa93a458c0
Bofeng Li <bofeng.li@outlook.com>
2019-03-25 23:46:12 +0800

    Update 218. The Skyline Problem

--- src/com/leetcode/array/No218TheSkylineProblem.java
+++ src/com/leetcode/array/No218TheSkylineProblem.java
@@ -3,38 +3,74 @@ package com.leetcode.array;
 import java.util.*;
 
 public class No218TheSkylineProblem {
+    // Use PriorityQueue
     public List<int[]> getSkyline(int[][] buildings) {
-        final List<int[]> lines = new ArrayList<>();
-        for (int[] building : buildings) {
-            lines.add(new int[]{building[0], building[2]});
-            lines.add(new int[]{building[1], -building[2]});
+        final List<int[]> list = new ArrayList<>();
+        for (int[] p : buildings) {
+            list.add(new int[]{p[0], p[2]});
+            list.add(new int[]{p[1], -p[2]});
         }
 
-        Collections.sort(lines, new Comparator<int[]>() {
-            public int compare(int[] A1, int[] A2) {
-                return A1[0] == A2[0] ? (A2[1] - A1[1]) : (A1[0] - A2[0]);
-            }
-        });
+        list.sort((a, b) -> a[0] == b[0] ? (b[1] - a[1]) : (a[0] - b[0]));
+        final Queue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        final List<int[]> result = new ArrayList<>();
 
-        final PriorityQueue<Integer> pq = new PriorityQueue<>((i1, i2) -> i2 - i1);
-        List<int[]> R = new ArrayList<>();
-        int pre = 0;
-        for (int[] line : lines) {
-            int max;
-            if (line[1] > 0) {
-                pq.offer(line[1]);
-                max = pq.peek();
+        int last = 0;
+        for (int[] p : list) {
+            int x = p[0], h = p[1], max = 0;
+            if (h > 0) pq.offer(h);
+            else pq.remove(-h);
+
+            max = pq.isEmpty() ? 0 : pq.peek();
+            if (max != last) {
+                result.add(new int[]{x, max});
+                last = max;
+            }
+        }
+
+        return result;
+    }
+
+    // Use TreeMap. Since there will be performance problem when removing item from PriorityQueue.
+    public List<int[]> getSkyline2(int[][] buildings) {
+        final List<int[]> result = new ArrayList<>();
+        final List<int[]> list = new ArrayList<>();
+        for (int[] b : buildings) {
+            list.add(new int[]{b[0], b[2]});
+            list.add(new int[]{b[1], -b[2]});
+        }
+        list.sort((a, b) -> a[0] == b[0] ? (b[1] - a[1]) : (a[0] - b[0]));
+
+        // If use a separate comparator will be more efficient by comparing to a lambda
+        /*
+        Comparator<int[]> comparator = new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0] == b[0]? (b[1] - a[1]) : (a[0] - b[0]);
+            }
+        };
+         */
+
+        int lastH = 0;
+
+        final TreeMap<Integer, Integer> treemap = new TreeMap<>();
+        for (int[] p : list) {
+            int x = p[0], h = p[1], maxH = 0;
+            if (h > 0) {
+                treemap.put(h, treemap.getOrDefault(h, 0) + 1);
+                maxH = treemap.lastKey();
             } else {
-                pq.remove(-line[1]);
-                max = pq.isEmpty() ? 0 : pq.peek();
+                h = -h;
+                treemap.put(h, treemap.get(h) - 1);
+                if (treemap.get(h) == 0) treemap.remove(h);
+                maxH = treemap.isEmpty() ? 0 : treemap.lastKey();
             }
 
-            if (max != pre) {
-                R.add(new int[]{line[0], max});
-                pre = max;
+            if (maxH != lastH) {
+                result.add(new int[]{x, maxH});
+                lastH = maxH;
             }
         }
 
-        return R;
+        return result;
     }
 }
