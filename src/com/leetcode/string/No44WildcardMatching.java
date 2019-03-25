8fb55b6857f7ca306c1b7ea8500342530f241226
Bofeng Li <bofeng.li@outlook.com>
2019-03-25 11:05:13 +0800

    Update 44. Wildcard Matching

--- src/com/leetcode/string/No44WildcardMatching.java
+++ src/com/leetcode/string/No44WildcardMatching.java
@@ -1,22 +1,40 @@
 package com.leetcode.string;
 
 public class No44WildcardMatching {
+    // TLE
     public boolean isMatch(String s, String p) {
-        return isMath(s, 0, p, 0);
+        if (p.isEmpty()) return s.isEmpty();
+        if (s.isEmpty()) return p.charAt(0) == '*' && isMatch(s, p.substring(1));
+
+        boolean firstMatch = s.charAt(0) == p.charAt(0) || p.charAt(0) == '?';
+        if (firstMatch) return isMatch(s.substring(1), p.substring(1));
+        else if (p.charAt(0) == '*') return isMatch(s, p.substring(1))
+                || isMatch(s.substring(1), p.substring(1))
+                || isMatch(s.substring(1), p);
+
+        return false;
     }
 
-    private boolean isMath(String s, int i, String p, int j) {
-        if (j == p.length()) return i == s.length();
-        if (i == s.length()) return p.charAt(j) == '*' && isMath(s, i, p, j + 1);
+    // TLE
+    public boolean isMatch2(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
 
-        if (p.charAt(j) == '*') return isMath(s, i, p, j + 1) || isMath(s, i + 1, p, j + 1) || isMath(s, i + 1, p, j);
-        else if (s.charAt(i) == p.charAt(j) || '?' == p.charAt(j)) return isMath(s, i + 1, p, j + 1);
+    private boolean isMatch(String s, int i, String p, int j) {
+        if (j == p.length()) return i == s.length();
+        if (i == s.length()) return p.charAt(j) == '*' && isMatch(s, i, p, j + 1);
+
+        boolean firstMatch = s.charAt(i) == p.charAt(j) || p.charAt(j) == '?';
+        if (firstMatch) return isMatch(s, i + 1, p, j + 1);
+        else if (p.charAt(j) == '*') return isMatch(s, i + 1, p, j + 1)
+                || isMatch(s, i, p, j + 1)
+                || isMatch(s, i + 1, p, j);
 
         return false;
     }
 
 
-    public boolean isMatch2(String s, String p) {
+    public boolean isMatch3(String s, String p) {
         final int m = s.length(), n = p.length();
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
@@ -37,7 +55,12 @@ public class No44WildcardMatching {
         No44WildcardMatching solution = new No44WildcardMatching();
         String s = "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb";
         String p = "**aa*****ba*a*bb**aa*ab****a*aaaaaa***";
-        boolean matched = solution.isMatch(s, p);
+        boolean matched = solution.isMatch2(s, p);
+        System.out.println("matched = " + matched);
+
+        s = "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab";
+        p = "***bba**a*bbba**aab**b";
+        matched = solution.isMatch2(s, p);
         System.out.println("matched = " + matched);
     }
 }
