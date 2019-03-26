dcc5293c4f74c5786e67c3bfe166c263756be2fd
Bofeng Li <bofeng.li@outlook.com>
2019-03-26 22:41:37 +0800

    371. Sum of Two Integers

---
+++ src/com/leetcode/bit/No371SumOfTwoIntegers.java
@@ -0,0 +1,45 @@
+package com.leetcode.bit;
+
+public class No371SumOfTwoIntegers {
+    // https://leetcode.com/problems/sum-of-two-integers/discuss/84290/Java-simple-easy-understand-solution-with-explanation
+
+    // Recursive
+    public int getSum(int a, int b) {
+        return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+    }
+
+    // Recursive
+    public int getSubtract(int a, int b) {
+        return (b == 0) ? a : getSubtract(a ^ b, (~a & b) << 1);
+    }
+
+    // Get negative number
+    private int negate(int x) {
+        return ~x + 1;
+    }
+
+    // Iterative
+    public int getSum2(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+    // Iterative
+    public int getSubtract2(int a, int b) {
+        while (b != 0) {
+            int borrow = (~a) & b;
+            a = a ^ b;
+            b = borrow << 1;
+        }
+
+        return a;
+    }
+}
