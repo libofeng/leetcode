d5ec7454a2d90e36f49baa9eb409001fa612aee2
Bofeng Li <bofeng.li@outlook.com>
2019-03-29 00:00:27 +0800

    Update 112. Path Sum.

--- src/com/leetcode/tree/No112PathSum.java
+++ src/com/leetcode/tree/No112PathSum.java
@@ -3,8 +3,10 @@ package com.leetcode.tree;
 public class No112PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
-        if (root.left == null && root.right == null && sum == root.val) return true;
 
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        sum -= root.val;
+        if (root.left == null && root.right == null) return sum == 0;
+
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 }
