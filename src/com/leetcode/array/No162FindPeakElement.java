994e672e3194a8a0b28acde0db859a47cf3e4b9e
Bofeng Li <bofeng.li@outlook.com>
2019-03-28 18:52:41 +0800

    Update 162. Find Peak Element

--- src/com/leetcode/array/No162FindPeakElement.java
+++ src/com/leetcode/array/No162FindPeakElement.java
@@ -1,7 +1,19 @@
 package com.leetcode.array;
 
 public class No162FindPeakElement {
+    // O(N)
     public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            long left = i - 1 < 0 ? Long.MIN_VALUE : nums[i - 1];
+            long right = i + 1 >= nums.length ? Long.MIN_VALUE : nums[i + 1];
+
+            if (nums[i] > left && nums[i] > right) return i;
+        }
+        return -1;
+    }
+
+    // O(LogN)
+    public int findPeakElement2(int[] nums) {
         int lo = 0, hi = nums.length - 1;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
