5943fe786829a23db75742f3a5273e377dc88b4d
Bofeng Li <bofeng.li@outlook.com>
2019-03-28 23:29:06 +0800

    Update 220. Contains Duplicate III

--- src/com/leetcode/array/No220ContainsDuplicateIII.java
+++ src/com/leetcode/array/No220ContainsDuplicateIII.java
@@ -3,7 +3,21 @@ package com.leetcode.array;
 import java.util.TreeSet;
 
 public class No220ContainsDuplicateIII {
+    // Brute force
+    // O(N^2)
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        final int n = nums.length;
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(Math.abs(nums[i] - nums[j])<=t && j - i<=k) return true;
+            }
+        }
+
+        return false;
+    }
+
+    //
+    public boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
         if (nums.length < 2 || k <= 0 || t < 0) return false;
 
         final TreeSet<Long> set = new TreeSet<>();
