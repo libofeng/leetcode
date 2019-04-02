a8d4f2c30a486319296d801be036efcfe8bbb34b
Bofeng Li <bofeng.li@outlook.com>
2019-04-02 15:18:31 +0800

    Update 210. Course Schedule II of using tarjan's algorithm.

--- src/com/leetcode/graph/topological/No210CourseScheduleII.java
+++ src/com/leetcode/graph/topological/No210CourseScheduleII.java
@@ -30,8 +30,8 @@ public class No210CourseScheduleII {
         for (int[] edge : prerequisites) inDegree[edge[0]]++;
 
         final List<List<Integer>> graph = new ArrayList<>();
-        for(int v = 0; v < numCourses; v++) graph.add(new ArrayList<>());
-        for(int[] edge : prerequisites) graph.get(edge[1]).add(edge[0]);
+        for (int v = 0; v < numCourses; v++) graph.add(new ArrayList<>());
+        for (int[] edge : prerequisites) graph.get(edge[1]).add(edge[0]);
 
         final Queue<Integer> q = new LinkedList<>();
         for (int v = 0; v < numCourses; v++) if (inDegree[v] == 0) q.offer(v);
@@ -85,38 +85,32 @@ public class No210CourseScheduleII {
 
     // use Adjacency list to optimize Tarjan's Algorithms
     public int[] findOrder22(int numCourses, int[][] prerequisites) {
-        final int[] FAIL = new int[0];
-        if (numCourses <= 0) return FAIL;
-        if (prerequisites == null || prerequisites.length == 0) {
-            int[] R = new int[numCourses];
-            for (int v = 0; v < numCourses; v++) R[v] = v;
-            return R;
-        }
+        final List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
+        for (int[] p : prerequisites) graph.get(p[1]).add(p[0]);
 
         final boolean[] visited = new boolean[numCourses];
         final boolean[] visiting = new boolean[numCourses];
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int v = 0; v < numCourses; v++) graph.add(new ArrayList<>());
-        for (int[] edge : prerequisites) graph.get(edge[1]).add(edge[0]);
-
         final Stack<Integer> stack = new Stack<>();
-        for (int v = 0; v < numCourses; v++) if (!visited[v] && !dfs(v, graph, visited, visiting, stack)) return FAIL;
 
-        int[] R = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) R[i] = stack.pop();
-        return R;
+        for (int i = 0; i < numCourses; i++) if (!dfs(graph, visited, visiting, stack, i)) return new int[0];
+
+        int index = 0;
+        final int[] result = new int[stack.size()];
+        while (!stack.isEmpty()) result[index++] = stack.pop();
+
+        return result;
     }
 
-    private boolean dfs(int v, List<List<Integer>> graph, boolean[] visited, boolean[] visiting, Stack<Integer> stack) {
-        if (visited[v]) return true;
+    private boolean dfs(List<List<Integer>> graph, boolean[] visited, boolean[] visiting, Stack<Integer> stack, int n) {
+        if (visited[n]) return true;
 
-        visiting[v] = true;
-        visited[v] = true;
+        visiting[n] = true;
+        for (int next : graph.get(n)) if (visiting[next] || !dfs(graph, visited, visiting, stack, next)) return false;
 
-        for (int av : graph.get(v)) if (visiting[av] || !dfs(av, graph, visited, visiting, stack)) return false;
-
-        visiting[v] = false;
-        stack.push(v);
+        visiting[n] = false;
+        visited[n] = true;
+        stack.push(n);
         return true;
     }
 
