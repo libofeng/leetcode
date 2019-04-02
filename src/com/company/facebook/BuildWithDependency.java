e982a055f11df39196bb7f68d2724613a6e699d5
Bofeng Li <bofeng.li@outlook.com>
2019-04-02 15:51:19 +0800

    Update Facebook problem of build with dependency.

--- src/com/company/facebook/BuildWithDependency.java
+++ src/com/company/facebook/BuildWithDependency.java
@@ -3,46 +3,28 @@ package com.company.facebook;
 import java.util.*;
 
 public class BuildWithDependency {
-    List<String> build(String m, Map<String, List<String>> map) {
-        Map<String, List<String>> graph = new HashMap<>();
-        Map<String, Integer> inDegree = new HashMap<>();
-        Set<String> visiting = new HashSet<>();
-        buildGraph(m, map, graph, inDegree, visiting);
+    // No need to use topological sort!!!
+    List<String> build(String m, Map<String, List<String>> dependencies) {
+        final Set<String> visited = new HashSet<>(), visiting = new HashSet<>();
+        final Stack<String> stack = new Stack<>();
+        if (!dfs(dependencies, visited, visiting, stack, m)) throw new RuntimeException("Cycle detected.");
 
-        Queue<String> q = new LinkedList<>();
-        for (Map.Entry<String, Integer> entry : inDegree.entrySet()) if (entry.getValue() == 0) q.offer(entry.getKey());
-
-        List<String> R = new LinkedList<>();
-        while (!q.isEmpty()) {
-            String M = q.poll();
-            R.add(M);
-            if (!graph.containsKey(M) || graph.get(M).isEmpty()) continue;
-
-            for (String child : graph.get(M)) {
-                inDegree.put(child, inDegree.get(child) - 1);
-                if (inDegree.get(child) == 0) q.offer(child);
-            }
-        }
-
-        return R;
+        return new ArrayList<>(stack);
     }
 
-    private void buildGraph(String m, Map<String, List<String>> map, Map<String,
-            List<String>> graph, Map<String, Integer> inDegree, Set<String> visiting) {
+    private boolean dfs(Map<String, List<String>> graph, Set<String> visited, Set<String> visiting, Stack<String> stack, String n) {
+        if (visited.contains(n)) return true;
+        visiting.add(n);
 
-        if (visiting.contains(m)) throw new RuntimeException("Cyclic dependence");
-        inDegree.putIfAbsent(m, 0);
-
-        if (!map.containsKey(m) || map.get(m).isEmpty()) return;
-        visiting.add(m);
-        for (String s : map.get(m)) {
-            graph.putIfAbsent(s, new LinkedList<>());
-            graph.get(s).add(m);
-            inDegree.put(m, inDegree.get(m) + 1);
-
-            buildGraph(s, map, graph, inDegree, visiting);
+        if (graph.containsKey(n)) {
+            for (String next : graph.get(n))
+                if (visiting.contains(next) || !dfs(graph, visited, visiting, stack, next)) return false;
         }
-        visiting.remove(m);
+
+        visiting.remove(n);
+        visited.add(n);
+        stack.push(n);
+        return true;
     }
 
     public static void main(String[] args) {
