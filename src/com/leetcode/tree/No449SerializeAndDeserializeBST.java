5f520d927b51fde82dc5d0e541d1a1658e3de78e
Bofeng Li <bofeng.li@outlook.com>
2019-04-02 23:34:23 +0800

    449. Serialize and Deserialize BST(Update by using the BST feature)

--- src/com/leetcode/tree/No449SerializeAndDeserializeBST.java
+++ src/com/leetcode/tree/No449SerializeAndDeserializeBST.java
@@ -1,30 +1,50 @@
 package com.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class No449SerializeAndDeserializeBST {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return root == null ? "#" : (root.val + "," + serialize(root.left) + "," + serialize(root.right));
+        final Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return "#";
+
+        stack.push(root);
+
+        final StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            sb.append(node.val).append(",");
+
+            // the order here is important
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data == null) return null;
+        if (data == null || "#".equals(data)) return null;
+        final Queue<Integer> q = new LinkedList<>();
+        for (String t : data.split(",")) q.offer(Integer.parseInt(t));
 
-        return build(data.split(","));
+        return build(q);
     }
 
-    private int p = 0;
+    private TreeNode build(Queue<Integer> q) {
+        if (q.isEmpty()) return null;
 
-    private TreeNode build(String[] tokens) {
-        if (p > tokens.length) return null;
 
-        String val = tokens[p++];
-        if ("#".equals(val)) return null;
+        TreeNode root = new TreeNode(q.poll());
+        final Queue<Integer> left = new LinkedList<>();
+        while (!q.isEmpty() && q.peek() < root.val) left.offer(q.poll());
 
-        final TreeNode root = new TreeNode(Integer.parseInt(val));
-        root.left = build(tokens);
-        root.right = build(tokens);
+        root.left = build(left);
+        root.right = build(q);
 
         return root;
     }
