251da9343b1405d54fd51c14a58fe3fcc3aa18c7
Bofeng Li <bofeng.li@outlook.com>
2019-04-07 17:41:40 +0800

    Update 5019. Video Stitching

--- src/com/leetcode/contest/contest131/No5019VideoStitching.java
+++ src/com/leetcode/contest/contest131/No5019VideoStitching.java
@@ -22,4 +22,19 @@ public class No5019VideoStitching {
 
         return nextEnd >= T ? (count + 1) : -1;
     }
+
+    public int videoStitching2(int[][] clips, int T) {
+        final int n = clips.length, INF = 1 << 30;
+        final int[] dp = new int[n];
+        Arrays.sort(clips, (a, b) -> a[1] - b[1]);
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = clips[i][0] == 0 ? 1 : INF;
+            for (int j = i - 1; j >= 0; j--) if (clips[i][0] <= clips[j][1]) dp[i] = Math.min(dp[i], dp[j] + 1);
+        }
+
+        int min = INF;
+        for (int i = 0; i < n; i++) if (clips[i][1] >= T) min = Math.min(min, dp[i]);
+        return min == INF ? -1 : min;
+    }
 }
