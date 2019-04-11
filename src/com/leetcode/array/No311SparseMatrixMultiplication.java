b71fefb4811ec36c0a384246f98462db782050d6
Bofeng Li <bofeng.li@outlook.com>
2019-04-11 21:38:51 +0800

    654. Sparse Matrix Multiplication

---
+++ src/com/leetcode/array/No311SparseMatrixMultiplication.java
@@ -0,0 +1,48 @@
+package com.leetcode.array;
+
+public class No311SparseMatrixMultiplication {
+    /*
+    Given two sparse matrices A and B, return the result of AB.
+
+    You may assume that A's column number is equal to B's row number.
+
+    Example:
+
+    Input:
+
+    A = [
+      [ 1, 0, 0],
+      [-1, 0, 3]
+    ]
+
+    B = [
+      [ 7, 0, 0 ],
+      [ 0, 0, 0 ],
+      [ 0, 0, 1 ]
+    ]
+
+    Output:
+
+         |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+    AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                      | 0 0 1 |
+     */
+
+    /**
+     * @param A: a sparse matrix
+     * @param B: a sparse matrix
+     * @return: the result of A * B
+     */
+    public int[][] multiply(int[][] A, int[][] B) {
+        final int m = A.length, t = A[0].length, n = B[0].length;
+        final int[][] result = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < t; k++) result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+
+        return result;
+    }
+}
