3383b6f808fef035806a087e0e8dada45c3d1b82
Bofeng Li <bofeng.li@outlook.com>
2019-04-27 11:44:18 +0800

    Update max depth of brackets.

--- src/com/company/amazon/MaxDepthBrackets.java
+++ src/com/company/amazon/MaxDepthBrackets.java
@@ -3,19 +3,17 @@ package com.company.amazon;
 public class MaxDepthBrackets {
 
     int maxDepth(String s) {
-        if (s == null || s.isEmpty()) return 0;
-
-        int maxDepth = 0, left = 0;
+        int maxDepth = 0, maxLeft = 0, left = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
-            if (c == '(') maxDepth = Math.max(maxDepth, ++left);
+            if (c == '(') maxLeft = Math.max(maxLeft, ++left);
             else {
                 if (left > 0) left--;
+                maxDepth = Math.max(maxDepth, maxLeft - left);
             }
         }
 
-        return maxDepth - left;
+        return maxDepth;
     }
 
     public static void main(String[] args) {
@@ -36,5 +34,11 @@ public class MaxDepthBrackets {
 
         d = solution.maxDepth("()))))");
         System.out.println("expected:1, d = " + d);
+
+        d = solution.maxDepth("(())((((");
+        System.out.println("expected:2, d = " + d);
+
+        d = solution.maxDepth("(())())))(())(()()))))))");
+        System.out.println("expected:2, d = " + d);
     }
 }
