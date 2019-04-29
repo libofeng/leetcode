e99d292abe0fe46cc2eb0f735c1d66572972d09e
Bofeng Li <bofeng.li@outlook.com>
2019-04-29 12:08:50 +0800

    821. Shortest Distance to a Character

--- src/com/leetcode/string/No821ShortestDistanceToACharacter.java
+++ src/com/leetcode/string/No821ShortestDistanceToACharacter.java
@@ -1,5 +1,6 @@
 package com.leetcode.string;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class No821ShortestDistanceToACharacter {
@@ -14,6 +15,26 @@ public class No821ShortestDistanceToACharacter {
             if (floor != null && ceiling != null) result[i] = Math.min(i - floor, ceiling - i);
             else if (floor != null) result[i] = i - floor;
             else if (ceiling != null) result[i] = ceiling - i;
+        }
+
+        return result;
+    }
+
+    public int[] shortestToChar2(String S, char C) {
+        final int n = S.length();
+        final int[] result = new int[n];
+        Arrays.fill(result, Integer.MAX_VALUE);
+
+        int lastIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (S.charAt(i) == C) lastIndex = i;
+            if (lastIndex >= 0) result[i] = i - lastIndex;
+        }
+
+        lastIndex = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (S.charAt(i) == C) lastIndex = i;
+            if (lastIndex >= 0) result[i] = Math.min(result[i], lastIndex - i);
         }
 
         return result;
