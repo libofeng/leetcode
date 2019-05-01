ee32218ada638dd15cea3d7fdaeba4a37ad42136
Bofeng Li <bofeng.li@outlook.com>
2019-05-01 21:57:55 +0800

    Update No57 Insert Interval

--- src/com/leetcode/interval/No57InsertInterval.java
+++ src/com/leetcode/interval/No57InsertInterval.java
@@ -1,27 +1,27 @@
 package com.leetcode.interval;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class No57InsertInterval {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        final List<Interval> list = new ArrayList<>();
-
         Interval current = newInterval;
-        for (Interval i : intervals) {
-            if (current.end < i.start) {
-                list.add(current);
-                current = i;
-            } else if (i.end < current.start) {
-                list.add(i);
+
+        final List<Interval> result = new ArrayList<>();
+        for (Interval next : intervals) {
+            if (current.end < next.start) {
+                result.add(current);
+                current = next;
+            } else if (current.start > next.end) {
+                result.add(next);
             } else {
-                current.start = Math.min(current.start, i.start);
-                current.end = Math.max(current.end, i.end);
+                current.start = Math.min(current.start, next.start);
+                current.end = Math.max(current.end, next.end);
             }
         }
+        result.add(current);
 
-        list.add(current);
-
-        return list;
+        return result;
     }
 }
