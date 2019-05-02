da6dfd3864a5a24c0f3120adb9e276e600680a12
Bofeng Li <bofeng.li@outlook.com>
2019-05-02 13:06:07 +0800

    Update 636. Exclusive Time of Functions

--- src/com/leetcode/interval/No636ExclusiveTimeofFunctions.java
+++ src/com/leetcode/interval/No636ExclusiveTimeofFunctions.java
@@ -6,19 +6,16 @@ import java.util.Stack;
 public class No636ExclusiveTimeofFunctions {
     public int[] exclusiveTime(int n, List<String> logs) {
         final Stack<int[]> stack = new Stack<>();
+        final int[] result = new int[n];
 
-        int[] result = new int[n];
         for (String log : logs) {
-            String[] data = log.split(":");
-            boolean end = "end".equals(data[1]);
-            int id = Integer.parseInt(data[0]), time = Integer.parseInt(data[2]);
+            String[] token = log.split(":");
+            int id = Integer.parseInt(token[0]), time = Integer.parseInt(token[2]);
 
-            if (end) {
-                int[] start = stack.pop();
-                int consumed = time - start[1] + 1;
-
-                result[id] += consumed;
-                if (!stack.isEmpty()) result[stack.peek()[0]] -= consumed;
+            if ("end".equals(token[1])) {
+                int start = stack.pop()[1], duration = time - start + 1;
+                result[id] += duration;
+                if (!stack.isEmpty()) result[stack.peek()[0]] -= duration;
             } else stack.push(new int[]{id, time});
         }
 
