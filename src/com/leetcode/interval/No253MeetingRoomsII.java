ccb742a2d31075b2962d994ecd05abd5d7a36f9e
Bofeng Li <bofeng.li@outlook.com>
2019-05-02 09:06:16 +0800

    Update No253 Meeting Rooms II

--- src/com/leetcode/interval/No253MeetingRoomsII.java
+++ src/com/leetcode/interval/No253MeetingRoomsII.java
@@ -1,19 +1,16 @@
 package com.leetcode.interval;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+import java.util.*;
 
 public class No253MeetingRoomsII {
     // Priority Queue
     public int minMeetingRooms(List<Interval> intervals) {
         intervals.sort((a, b) -> a.start - b.start);
 
-        final PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (Interval i : intervals) {
-            if (!pq.isEmpty() && i.start > pq.peek()) pq.poll();
-            pq.offer(i.end);
+        final Queue<Integer> pq = new PriorityQueue<>();
+        for (Interval next : intervals) {
+            if (!pq.isEmpty() && next.start > pq.peek()) pq.poll();
+            pq.offer(next.end);
         }
 
         return pq.size();
@@ -21,36 +18,30 @@ public class No253MeetingRoomsII {
 
     // SweepLine(sorted list)
     public int minMeetingRooms2(List<Interval> intervals) {
-        List<int[]> points = new ArrayList<>();
+        final List<int[]> list = new ArrayList<>();
         for (Interval i : intervals) {
-            points.add(new int[]{i.start, 1});
-            points.add(new int[]{i.end, -1});
+            list.add(new int[]{i.start, 1});
+            list.add(new int[]{i.end, -1});
         }
+        list.sort((a, b) -> a[0] == b[0] ? (a[1] - b[1]) : (a[0] - b[0]));
 
-        points.sort((a, b) -> a[0] == b[0] ? (b[1] - a[1]) : (a[0] - b[0]));
-        int maxCount = 0, count = 0;
-        for (int[] p : points) {
-            count += p[1];
-            maxCount = Math.max(maxCount, count);
-        }
+        int max = 0, h = 0;
+        for (int[] p : list) max = Math.max(max, h += p[1]);
 
-        return maxCount;
+        return max;
     }
 
     // SweepLine(sorted map)
     public int minMeetingRooms3(List<Interval> intervals) {
-        final TreeMap<Integer, Integer> map = new TreeMap<>();
+        final Map<Integer, Integer> tm = new TreeMap<>();
         for (Interval i : intervals) {
-            map.put(i.start, map.getOrDefault(i.start, 0) + 1);
-            map.put(i.end + 1, map.getOrDefault(i.end + 1, 0) - 1);
+            tm.put(i.start, tm.getOrDefault(i.start, 0) + 1);
+            tm.put(i.end, tm.getOrDefault(i.end, 0) - 1);
         }
 
-        int maxCount = 0, count = 0;
-        for (int c : map.values()) {
-            count += c;
-            maxCount = Math.max(maxCount, count);
-        }
+        int max = 0, h = 0;
+        for (int v : tm.values()) max = Math.max(max, h += v);
 
-        return maxCount;
+        return max;
     }
 }
