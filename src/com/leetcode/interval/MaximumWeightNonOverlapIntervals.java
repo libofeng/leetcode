538a15cd9a7c7e7bfaa9455e1851ee9a8a4385aa
Bofeng Li <bofeng.li@outlook.com>
2019-05-03 20:46:17 +0800

    Maximum Weight Non-overlap Intervals

--- src/com/leetcode/interval/MaximumWeightNonOverlapIntervals.java
+++ src/com/leetcode/interval/MaximumWeightNonOverlapIntervals.java
@@ -3,45 +3,37 @@ package com.leetcode.interval;
 import java.util.List;
 
 public class MaximumWeightNonOverlapIntervals {
+    // Time: O(2^N), Space: O(2^N)
     int maxWeight(List<WeightedInterval> intervals) {
         intervals.sort((a, b) -> a.end - b.end);
-        return helper(intervals, intervals.size() - 1);
+        return helper(intervals, intervals.size());
     }
 
-    private int helper(List<WeightedInterval> intervals, int n) {
-        if (n < 0) return 0;
-        final WeightedInterval interval = intervals.get(n);
+    private int helper(List<WeightedInterval> intervals, int i) {
+        if (i == 0) return 0;
+        final WeightedInterval interval = intervals.get(i - 1);
 
-        int i = n - 1;
-        while (i >= 0) {
-            WeightedInterval prev = intervals.get(i);
-            if (prev.end < interval.start) break;
+        int j = i - 1;
+        for (; j > 0; j--) if (intervals.get(j - 1).end < interval.start) break;
 
-            i--;
-        }
-
-        return Math.max(interval.weight + helper(intervals, i), helper(intervals, n - 1));
+        return Math.max(interval.weight + helper(intervals, j), helper(intervals, i - 1));
     }
 
-
+    // DP
+    // Time: O(N^2), Space: O(N)
     int maxWeight2(List<WeightedInterval> intervals) {
         intervals.sort((a, b) -> a.end - b.end);
         final int n = intervals.size();
-        final int[] dp = new int[n];
+        final int[] dp = new int[n + 1];
 
-        int maxWeight = 0;
-        for (int i = 0; i < n; i++) {
-            WeightedInterval interval = intervals.get(i);
-            dp[i] = interval.weight;
+        for (int i = 1; i <= n; i++) {
+            WeightedInterval interval = intervals.get(i - 1);
 
-            for (int j = i - 1; j >= 0; j--) {
-                WeightedInterval prev = intervals.get(j);
-                if (prev.end < interval.start) dp[i] = Math.max(dp[i], dp[j] + interval.weight);
-            }
-
-            maxWeight = Math.max(maxWeight, dp[i]);
+            int j = i - 1;
+            for (; j > 0; j--) if (intervals.get(j - 1).end < interval.start) break;
+            dp[i] = Math.max(dp[i - 1], dp[j] + interval.weight);
         }
 
-        return maxWeight;
+        return dp[n];
     }
 }
