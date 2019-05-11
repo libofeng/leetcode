57478d4d4ee95de792f1e45b04d5845eac32cc3c
Bofeng Li <bofeng.li@outlook.com>
2019-05-11 21:41:50 +0800

    674. Longest Continuous Increasing Subsequence

--- src/com/leetcode/array/No674LongestContinuousIncreasingSubsequence.java
+++ src/com/leetcode/array/No674LongestContinuousIncreasingSubsequence.java
@@ -3,12 +3,11 @@ package com.leetcode.array;
 public class No674LongestContinuousIncreasingSubsequence {
     public int findLengthOfLCIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        int len = 1, maxLen = 1;
+
+        int maxLen = 1, len = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                len++;
-                maxLen = Math.max(maxLen, len);
-            } else len = 1;
+            if (nums[i] > nums[i - 1]) maxLen = Math.max(maxLen, ++len);
+            else len = 1;
         }
 
         return maxLen;
