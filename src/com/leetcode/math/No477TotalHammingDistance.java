7b8aaf164ec3ce9e8fd4db0fe67603baa59a1792
Bofeng Li <bofeng.li@outlook.com>
2019-05-11 19:51:25 +0800

    477. Total Hamming Distance

--- src/com/leetcode/math/No477TotalHammingDistance.java
+++ src/com/leetcode/math/No477TotalHammingDistance.java
@@ -1,6 +1,8 @@
 package com.leetcode.math;
 
 public class No477TotalHammingDistance {
+    // Time: O(N), Space: O(1)
+    // distance of a bit: ones * not-ones
     public int totalHammingDistance(int[] nums) {
         int total = 0;
         for (int i = 0; i < 32; i++) {
@@ -10,5 +12,23 @@ public class No477TotalHammingDistance {
         }
 
         return total;
+    }
+
+    // Brute force, TLE
+    // Time: O(N^2), Space: O(1)
+    public int totalHammingDistance2(int[] nums) {
+        int d = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                d += distance(nums[i], nums[j]);
+            }
+        }
+        return d;
+    }
+
+    private int distance(int a, int b) {
+        int n = a ^ b, d = 0;
+        for (int i = 0; i < 32; i++) if (((n >> i) & 1) == 1) d++;
+        return d;
     }
 }
