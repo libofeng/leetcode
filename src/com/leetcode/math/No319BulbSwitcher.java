673262b3026803ce2f97934d28c32df5a5e0cab5
Bofeng Li <bofeng.li@outlook.com>
2019-05-14 11:07:24 +0800

    319. Bulb Switcher

---
+++ src/com/leetcode/math/No319BulbSwitcher.java
@@ -0,0 +1,8 @@
+package com.leetcode.math;
+
+public class No319BulbSwitcher {
+    // https://leetcode.com/problems/bulb-switcher/discuss/77112/Share-my-o(1)-solution-with-explanation
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
