e0c68e1c2f2ff0ac29db21578bbd9897ad1c0f96
Bofeng Li <bofeng.li@outlook.com>
2019-05-18 00:19:09 +0800

    Update find kth missing number.

--- src/com/leetcode/KthMissingNumber.java
+++ src/com/leetcode/KthMissingNumber.java
@@ -6,26 +6,31 @@ public class KthMissingNumber {
     比如给定原数组：[2,4,7,8,9,15]，漏掉的数组成这样一个虚拟数组：[3,5,6,10,11,12,13,14]。若k=2，返回虚拟数组的第二个数“5”。
      */
 
+    public static void main(String[] args) {
+        KthMissingNumber kthMissingNumber = new KthMissingNumber();
+        int n = kthMissingNumber.findKthMissing(new int[]{2, 4, 7, 8, 9, 15}, 2);
+        System.out.println("n = " + n + ", expected: 5");
+    }
+
+    // solution: Binary search and make 2 pointers stop at a pair of adjacent numbers.
+    // if we can find the kth number from the missing numbers between the 2 pointers, return the result
+    // otherwise return -1
     public int findKthMissing(int[] nums, int k) {
-        if (nums == null || nums.length < 2) {
-            return -1;
+        if (nums == null || nums.length < 2) return -1;
+
+        final int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2, missingCount = (nums[mid] - nums[lo]) - (mid - lo);
+
+            if (missingCount < k) {
+                lo = mid;
+                k -= missingCount;
+            } else hi = mid;
         }
-        int left = 0;
-        int right = nums.length - 1;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            int all = (nums[mid] - nums[left] + 1) - (mid - left + 1);
-            if (k > all) {
-                k -= all;
-                left = mid;
-            } else {
-                right = mid;
-            }
-        }
-        if (nums[left] + k >= nums[right]) {
-            return -1;
-        } else {
-            return nums[left] + k;
-        }
+
+        // the 2 pointers stop at the adjacent 2 numbers
+        if (nums[lo] + k >= nums[hi]) return -1;
+        return nums[lo] + k;
     }
 }
