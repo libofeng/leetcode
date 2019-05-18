6376e5c16d59c9eb3b667f8843b4a2970db12805
Bofeng Li <bofeng.li@outlook.com>
2019-05-18 23:42:00 +0800

    448. Find All Numbers Disappeared in an Array

--- src/com/leetcode/array/No448FindAllNumbersDisappearedInAnArray.java
+++ src/com/leetcode/array/No448FindAllNumbersDisappearedInAnArray.java
@@ -1,10 +1,29 @@
 package com.leetcode.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class No448FindAllNumbersDisappearedInAnArray {
+
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        final List<Integer> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = 1, i = 0;
+        while (i < nums.length) {
+            if (n == nums[i]) {
+                i++;
+                n++;
+            } else if (n < nums[i]) result.add(n++);
+            else i++;
+        }
+        while (n <= nums.length) result.add(n++);
+
+        return result;
+    }
+
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             int index = Math.abs(nums[i]) - 1;
