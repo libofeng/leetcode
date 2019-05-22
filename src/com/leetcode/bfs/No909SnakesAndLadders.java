f80a81da2fac8454a1acb6cadb8f0e3255c0fc33
Bofeng Li <bofeng.li@outlook.com>
2019-05-23 01:03:42 +0800

    909. Snakes and Ladders

--- src/com/leetcode/bfs/No909SnakesAndLadders.java
+++ src/com/leetcode/bfs/No909SnakesAndLadders.java
@@ -5,36 +5,11 @@ import java.util.Queue;
 
 public class No909SnakesAndLadders {
 
-    public int snakesAndLadders(int[][] board) {
-        final int m = board.length, n = m == 0 ? 0 : board[0].length, total = m * n;
-        final boolean[] visited = new boolean[total];
-        final Queue<Integer> q = new LinkedList<>();
-
-        q.offer(1);
-        visited[0] = true;
-
-        int moves = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                int index = q.poll();
-
-                for (int i = 1; i <= 6; i++) {
-                    int next = index + i;
-                    int[] p = getPosition(m, n, next - 1);
-                    if (board[p[0]][p[1]] >= 0) next = board[p[0]][p[1]];
-
-                    if (next == total) return moves + 1;
-                    if (next > total || visited[next - 1]) continue;
-
-                    visited[next - 1] = true;
-                    q.offer(next);
-                }
-            }
-            moves++;
-        }
-
-        return -1;
+    public static void main(String[] args) {
+        No909SnakesAndLadders solution = new No909SnakesAndLadders();
+        int moves = solution.snakesAndLadders(new int[][]{{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 35, -1, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}});
+//        int moves = solution.snakesAndLadders(new int[][]{{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}});
+        System.out.println("moves = " + moves);
     }
 
     private int[] getPosition(int rows, int cols, int index) {
@@ -45,10 +20,36 @@ public class No909SnakesAndLadders {
         return new int[]{x, y};
     }
 
-    public static void main(String[] args) {
-        No909SnakesAndLadders solution = new No909SnakesAndLadders();
-        int moves = solution.snakesAndLadders(new int[][]{{-1, -1, 2, -1}, {14, 2, 12, 3}, {4, 9, 1, 11}, {-1, 2, 1, 16}});
-//        int moves = solution.snakesAndLadders(new int[][]{{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}});
-        System.out.println("moves = " + moves);
+    public int snakesAndLadders(int[][] board) {
+        final int m = board.length, n = m == 0 ? 0 : board[0].length, total = m * n;
+
+        final Queue<Integer> q = new LinkedList<>();
+        final boolean[] visited = new boolean[total];
+        q.offer(1);
+        visited[0] = true;
+
+        int steps = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int index = q.poll();
+                if (index == total) return steps;
+
+                for (int i = 1; i <= 6 && index + i <= total; i++) {
+                    int next = index + i;
+
+                    int[] p = getPosition(m, n, next - 1);
+                    if (board[p[0]][p[1]] > 0) next = board[p[0]][p[1]];
+
+                    if (visited[next - 1]) continue;
+                    visited[next - 1] = true;
+
+                    q.offer(next);
+                }
+            }
+            steps++;
+        }
+
+        return -1;
     }
 }
