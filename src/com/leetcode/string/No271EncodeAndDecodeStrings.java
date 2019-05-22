f89b771ff399783b5da7122e59d0f0473cb53d9b
Bofeng Li <bofeng.li@outlook.com>
2019-05-22 10:07:37 +0800

    271. Encode and Decode Strings

--- src/com/leetcode/string/No271EncodeAndDecodeStrings.java
+++ src/com/leetcode/string/No271EncodeAndDecodeStrings.java
@@ -5,31 +5,25 @@ import java.util.List;
 
 public class No271EncodeAndDecodeStrings {
     // https://www.cnblogs.com/airwindow/p/4799854.html
-
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        if (strs == null) return null;
-
         final StringBuilder sb = new StringBuilder();
-        for (String str : strs) sb.append(str.length()).append(":").append(str);
-
+        for (String s : strs) sb.append(s.length()).append(":").append(s);
         return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        if (s == null) return null;
-
-        final List<String> strs = new ArrayList<>();
-        int startIndex = 0;
-        while (startIndex < s.length()) {
-            int index = s.indexOf(":", startIndex);
-            int len = Integer.parseInt(s.substring(startIndex, index));
-            strs.add(s.substring(index + 1, index + 1 + len));
-            startIndex = index + 1 + len;
+        final List<String> result = new ArrayList<>();
+        int start = 0;
+        while (start < s.length()) {
+            int index = s.indexOf(":", start);
+            int len = Integer.parseInt(s.substring(start, index));
+            result.add(s.substring(index + 1, index + len + 1));
+            start = index + len + 1;
         }
 
-        return strs;
+        return result;
     }
 
     public static void main(String[] args) {
