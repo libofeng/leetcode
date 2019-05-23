44ee940d80105f9160453e5ed738a939e7008d9f
Bofeng Li <bofeng.li@outlook.com>
2019-05-23 14:36:27 +0800

    82. Remove Duplicates from Sorted List II

--- src/com/leetcode/list/No82RemoveDuplicatesFromSortedListII.java
+++ src/com/leetcode/list/No82RemoveDuplicatesFromSortedListII.java
@@ -1,37 +1,37 @@
 package com.leetcode.list;
 
 public class No82RemoveDuplicatesFromSortedListII {
-    public ListNode deleteDuplicates2(ListNode head) {
-        ListNode dummy = new ListNode(0), tail = dummy;
-
-        while (head != null) {
-            if (head.next != null && head.val == head.next.val) {
-                int duplicateValue = head.val;
-                while (head != null && head.val == duplicateValue) head = head.next;
-            } else {
-                ListNode node = head;
-                head = head.next;
-                node.next = null;
-
-                tail.next = node;
-                tail = node;
-            }
-        }
-
-        return dummy.next;
-    }
 
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        ListNode next = head.next;
-        if (head.val == next.val) {
+        if (head.val == head.next.val) {
+            ListNode next = head.next;
             while (next != null && next.val == head.val) next = next.next;
             return deleteDuplicates(next);
-        } else {
-            head.next = deleteDuplicates(next);
         }
 
+        head.next = deleteDuplicates(head.next);
         return head;
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode dummyHead = new ListNode(0), p = dummyHead;
+
+        while (head != null) {
+            ListNode next = head.next;
+            while (next != null && next.val == head.val) next = next.next;
+
+            if (next == head.next) {
+                head.next = null;
+
+                p.next = head;
+                p = p.next;
+            }
+
+            head = next;
+        }
+
+        return dummyHead.next;
     }
 }
