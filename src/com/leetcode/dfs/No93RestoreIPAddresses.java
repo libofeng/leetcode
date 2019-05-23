46aa3fc1fc60ebd9333d7c30ecd5e56078c789fa
Bofeng Li <bofeng.li@outlook.com>
2019-05-23 15:29:55 +0800

    93. Restore IP Addresses

--- src/com/leetcode/dfs/No93RestoreIPAddresses.java
+++ src/com/leetcode/dfs/No93RestoreIPAddresses.java
@@ -1,39 +1,35 @@
 package com.leetcode.dfs;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class No93RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
-        final List<String> R = new LinkedList<>();
-        if (s.length() > 12) return R;
+        final List<String> result = new ArrayList<>();
+        if (s.length() > 12) return result;
 
-        dfs(s, R, 0, new LinkedList<>());
-
-        return R;
+        dfs(s, 0, 0, result, "");
+        return result;
     }
 
-    private void dfs(String s, List<String> R, int start, LinkedList<String> list) {
-        if (list.size() == 4) {
-            if (start != s.length()) return;
-
-            StringBuilder builder = new StringBuilder();
-            for (String i : list) builder.append(i).append(".");
-            builder.deleteCharAt(builder.length() - 1);
-            R.add(builder.toString());
+    private void dfs(String s, int start, int level, List<String> result, String ip) {
+        if (start == s.length() || level == 4) {
+            if (start == s.length() && level == 4) result.add(ip);
             return;
         }
 
         for (int i = start; i < start + 3 && i < s.length(); i++) {
-            String item = s.substring(start, i + 1);
-            if (i > start && s.charAt(start) == '0') break;
+            if (i > start && s.charAt(start) == '0') return;
 
-            int n = Integer.parseInt(item);
-            if (n < 0 || n > 255) break;
+            String token = s.substring(start, i + 1);
+            int num = Integer.parseInt(token);
+            if (num >= 0 && num <= 255) {
+                String nextIp = ip;
+                if (level > 0) nextIp += ".";
+                nextIp += token;
 
-            list.add(item);
-            dfs(s, R, i + 1, list);
-            list.removeLast();
+                dfs(s, i + 1, level + 1, result, nextIp);
+            }
         }
     }
 }
