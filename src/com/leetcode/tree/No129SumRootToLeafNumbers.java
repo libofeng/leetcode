c48d759d9651667c79a74a421991b387539eaf45
Bofeng Li <bofeng.li@outlook.com>
2019-05-25 23:00:01 +0800

    129. Sum Root to Leaf Numbers

--- src/com/leetcode/tree/No129SumRootToLeafNumbers.java
+++ src/com/leetcode/tree/No129SumRootToLeafNumbers.java
@@ -12,4 +12,17 @@ public class No129SumRootToLeafNumbers {
 
         return preOrder(root.left, sum + root.val) + preOrder(root.right, sum + root.val);
     }
+
+    public int sumNumbers2(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int sum) {
+        if (root == null) return 0;
+        sum = sum * 10 + root.val;
+        if (root.left == null && root.right == null) return sum;
+
+        int left = dfs(root.left, sum), right = dfs(root.right, sum);
+        return left + right;
+    }
 }
