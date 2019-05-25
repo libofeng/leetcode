03caa3e0c40f17c39f52778c357d998320e3da66
Bofeng Li <bofeng.li@outlook.com>
2019-05-25 23:40:36 +0800

    131. Palindrome Partitioning

--- src/com/leetcode/dfs/No131PalindromePartitioning.java
+++ src/com/leetcode/dfs/No131PalindromePartitioning.java
@@ -1,9 +1,11 @@
 package com.leetcode.dfs;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class No131PalindromePartitioning {
+    // Time: O(N!), Space: O(N)
     public List<List<String>> partition(String s) {
         final List<List<String>> R = new LinkedList<>();
         dfs(s, 0, R, new LinkedList<>());
@@ -28,5 +30,33 @@ public class No131PalindromePartitioning {
     private boolean isPalindrome(String s, int i, int j) {
         while (i < j) if (s.charAt(i++) != s.charAt(j--)) return false;
         return true;
+    }
+
+
+    // Use DP to pre-calculate the valid palindrome substring.
+    // Time: O(N!), Space: O(N)
+    public List<List<String>> partition2(String s) {
+        final int n = s.length();
+        final boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 1 || dp[j + 1][i - 1])) dp[j][i] = true;
+            }
+        }
+
+        final List<List<String>> result = new ArrayList<>();
+        dfs(s, 0, dp, result, new ArrayList<>());
+        return result;
+    }
+
+    private void dfs(String s, int start, boolean[][] dp, List<List<String>> result, List<String> list) {
+        if (start == s.length()) result.add(new ArrayList<>(list));
+
+        for (int i = start; i < s.length(); i++) {
+            if (!dp[start][i]) continue;
+            list.add(s.substring(start, i + 1));
+            dfs(s, i + 1, dp, result, list);
+            list.remove(list.size() - 1);
+        }
     }
 }
