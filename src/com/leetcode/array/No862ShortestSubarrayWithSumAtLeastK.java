9c8a52a013853a61146808834339aff75f9cc12d
Bofeng Li <bofeng.li@outlook.com>
2019-05-31 01:37:37 +0800

    862. Shortest Subarray with Sum at Least K

---
+++ src/com/leetcode/array/No862ShortestSubarrayWithSumAtLeastK.java
@@ -0,0 +1,42 @@
+package com.leetcode.array;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class No862ShortestSubarrayWithSumAtLeastK {
+    // TLE
+    // Time: O(N^2), Space: O(N)
+    public int shortestSubarray(int[] A, int K) {
+        final int n = A.length;
+        final int[] sums = new int[n + 1];
+        for (int i = 1; i <= n; i++) sums[i] = sums[i - 1] + A[i - 1];
+
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sums[i] - sums[j] >= K) minLen = Math.min(minLen, i - j);
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? -1 : minLen;
+    }
+
+    // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/143726/C%2B%2BJavaPython-O(N)-Using-Deque
+    public int shortestSubarray2(int[] A, int K) {
+        final int n = A.length;
+        final int[] sums = new int[n + 1];
+        for (int i = 1; i <= n; i++) sums[i] = sums[i - 1] + A[i - 1];
+
+        final Deque<Integer> dq = new LinkedList<>();
+        int minLen = n + 1;
+        for (int i = 0; i <= n; i++) {
+            while (!dq.isEmpty() && sums[i] - sums[dq.peek()] >= K) {
+                minLen = Math.min(minLen, i - dq.poll());
+            }
+            while (!dq.isEmpty() && sums[i] <= sums[dq.peekLast()]) dq.pollLast();
+            dq.offer(i);
+        }
+
+        return minLen == n + 1 ? -1 : minLen;
+    }
+}
