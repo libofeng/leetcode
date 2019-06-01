7d88ebe82bd33ed2b7d84bb49f740aa69112c2a6
Bofeng Li <bofeng.li@outlook.com>
2019-06-01 22:43:41 +0800

    274. H-Index

--- src/com/leetcode/array/No274HIndex.java
+++ src/com/leetcode/array/No274HIndex.java
@@ -8,4 +8,20 @@ public class No274HIndex {
         for (int i = citations.length - 1, h = 0; i >= 0; i--) if (citations[i] < ++h) return h - 1;
         return citations.length;
     }
+
+    public int hIndex2(int[] citations) {
+        if (citations.length == 0) return 0;
+        Arrays.sort(citations);
+
+        final int n = citations.length;
+        int lo = 0, hi = n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if ((n - mid) > citations[mid]) lo = mid + 1;
+            else hi = mid;
+        }
+
+        int hIndex = n - lo;
+        return citations[lo] >= hIndex ? hIndex : (hIndex - 1);
+    }
 }
