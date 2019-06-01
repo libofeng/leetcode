4497d10c35a7454b42a03941154e62fb1bd1aa0b
Bofeng Li <bofeng.li@outlook.com>
2019-06-01 23:18:05 +0800

    528. Random Pick with Weight

--- src/com/leetcode/array/No528RandomPickWithWeight.java
+++ src/com/leetcode/array/No528RandomPickWithWeight.java
@@ -3,25 +3,26 @@ package com.leetcode.array;
 import java.util.Random;
 
 public class No528RandomPickWithWeight {
-    private int[] sum;
-    private Random rnd = new Random();
+    private final int[] sums;
+    private final Random rnd = new Random();
 
     public No528RandomPickWithWeight(int[] w) {
-        sum = new int[w.length + 1];
-        for (int i = 1; i < sum.length; i++) sum[i] = sum[i - 1] + w[i - 1];
+        sums = new int[w.length + 1];
+        for (int i = 1; i < sums.length; i++) sums[i] = sums[i - 1] + w[i - 1];
     }
 
     public int pickIndex() {
-        int w = rnd.nextInt(sum[sum.length - 1]);
+        final int w = rnd.nextInt(sums[sums.length - 1]);
 
-        int lo = 0, hi = sum.length - 1;
-        while (lo + 1 < hi) {
+        int lo = 1, hi = sums.length - 1;
+        while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
+            if (w < sums[mid] && w >= sums[mid - 1]) return mid - 1;
 
-            if (w >= sum[mid]) lo = mid;
+            if (w >= sums[mid]) lo = mid + 1;
             else hi = mid - 1;
         }
 
-        return w >= sum[hi] ? hi : (hi - 1);
+        return 0;
     }
 }
