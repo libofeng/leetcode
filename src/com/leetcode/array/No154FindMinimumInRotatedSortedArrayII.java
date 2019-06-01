722b2501f6e4a00d437be020ff098c7a350716d0
Bofeng Li <bofeng.li@outlook.com>
2019-06-01 22:09:54 +0800

    154. Find Minimum in Rotated Sorted Array II

--- src/com/leetcode/array/No154FindMinimumInRotatedSortedArrayII.java
+++ src/com/leetcode/array/No154FindMinimumInRotatedSortedArrayII.java
@@ -1,5 +1,20 @@
 package com.leetcode.array;
 
 public class No154FindMinimumInRotatedSortedArrayII {
-    // TODO: TBD
+    // Binary search
+    // worse case: O(N)
+    public int findMin(int[] nums) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[lo] == nums[hi]) lo++;
+            else if (nums[lo] < nums[hi]) return nums[lo];
+            else {
+                if (nums[mid] >= nums[lo]) lo = mid + 1;
+                else hi = mid;
+            }
+        }
+
+        return nums[lo];
+    }
 }
