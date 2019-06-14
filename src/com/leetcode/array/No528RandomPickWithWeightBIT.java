c3cbb3d78240551c8a1b5219af3483dae6081498
Bofeng Li <bofeng.li@outlook.com>
2019-06-14 20:59:45 +0800

    528. Random Pick with Weight

--- src/com/leetcode/array/No528RandomPickWithWeightBIT.java
+++ src/com/leetcode/array/No528RandomPickWithWeightBIT.java
@@ -5,17 +5,56 @@ import java.util.TreeMap;
 
 public class No528RandomPickWithWeightBIT {
 
-    private TreeMap<Integer, Integer> tm = new TreeMap<>();
+
     private Random rnd = new Random();
-    private int sum = 0;
+    private BIT bit;
+    private int n;
+
     public No528RandomPickWithWeightBIT(int[] w) {
-        for(int i = 0;i<w.length;i++) {
-            sum += w[i];
-            tm.put(sum, i);
-        }
+        bit = new BIT(w);
+        n = w.length;
     }
 
     public int pickIndex() {
-        return tm.ceilingEntry(rnd.nextInt(sum) + 1).getValue();
+        int sum = rnd.nextInt(bit.query(n - 1)) + 1;
+
+        int lo = 0, hi = n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2, midSum = bit.query(mid), leftMidSum = bit.query(mid - 1);
+            if (sum > leftMidSum && sum <= midSum) return mid;
+
+            if (sum > midSum) lo = mid + 1;
+            else hi = mid - 1;
+        }
+
+        return lo;
+    }
+
+    class BIT {
+        int[] w, bit;
+
+        BIT(int[] w) {
+            this.w = w;
+            this.bit = new int[w.length + 1];
+
+            for (int i = 0; i < w.length; i++) set(i, w[i]);
+        }
+
+        void set(int i, int val) {
+            for (i++; i < bit.length; i += (i & -i)) bit[i] += val;
+        }
+
+        int query(int i) {
+            int sum = 0;
+            for (i++; i > 0; i -= (i & -i)) sum += bit[i];
+            return sum;
+        }
+
+        void update(int i, int val) {
+            int delta = val - w[i];
+            w[i] = val;
+
+            set(i, val);
+        }
     }
 }
--- src/com/leetcode/array/No528RandomPickWithWeightTreeMap.java
+++ src/com/leetcode/array/No528RandomPickWithWeightTreeMap.java
@@ -7,15 +7,13 @@ public class No528RandomPickWithWeightTreeMap {
 
     private TreeMap<Integer, Integer> tm = new TreeMap<>();
     private Random rnd = new Random();
-    private int sum = 0;
+    private int totalSum = 0;
+
     public No528RandomPickWithWeightTreeMap(int[] w) {
-        for(int i = 0;i<w.length;i++) {
-            sum += w[i];
-            tm.put(sum, i);
-        }
+        for (int i = 0; i < w.length; i++) tm.put((totalSum += w[i]), i);
     }
 
     public int pickIndex() {
-        return tm.ceilingEntry(rnd.nextInt(sum) + 1).getValue();
+        return tm.ceilingEntry(rnd.nextInt(totalSum) + 1).getValue();
     }
 }
