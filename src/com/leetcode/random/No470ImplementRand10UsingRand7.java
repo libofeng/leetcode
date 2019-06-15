a5e19e18bf38e944e932d78d5d24db592bb8405b
Bofeng Li <bofeng.li@outlook.com>
2019-06-15 22:39:08 +0800

    470. Implement Rand10() Using Rand7()

--- src/com/leetcode/random/No470ImplementRand10UsingRand7.java
+++ src/com/leetcode/random/No470ImplementRand10UsingRand7.java
@@ -4,14 +4,9 @@ public class No470ImplementRand10UsingRand7 {
     // https://www.cnblogs.com/liaohuiqiang/p/9857273.html
     // https://leetcode.com/problems/implement-rand10-using-rand7/discuss/151567/C%2B%2BJavaPython-Average-1.199-Call-rand7-Per-rand10
     public int rand10() {
-        int n = rand49();
-        while (n > 40) n = rand49();
-
-        return (n - 1) % 10 + 1;
-    }
-
-    private int rand49() {
-        return (rand7() - 1) * 7 + rand7();
+        int rnd = Integer.MAX_VALUE;
+        while (rnd >= 40) rnd = 7 * (rand7() - 1) + (rand7() - 1);
+        return rnd % 10 + 1;
     }
 
     private int rand7() {
