e950fa2c5a35e5b8a23e76d8da74f8829d333b87
Bofeng Li <bofeng.li@outlook.com>
2019-06-18 22:49:08 +0800

    892. Surface Area of 3D Shapes

---
+++ src/com/leetcode/geometry/No892SurfaceAreaOf3DShapes.java
@@ -0,0 +1,19 @@
+package com.leetcode.geometry;
+
+public class No892SurfaceAreaOf3DShapes {
+    public int surfaceArea(int[][] grid) {
+        int surface = 0;
+        final int m = grid.length, n = m == 0 ? 0 : grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) continue;
+
+                surface += grid[i][j] * 4 + 2;
+                if (i > 0) surface -= Math.min(grid[i - 1][j], grid[i][j]);
+                if (j > 0) surface -= Math.min(grid[i][j - 1], grid[i][j]);
+            }
+        }
+
+        return surface;
+    }
+}
