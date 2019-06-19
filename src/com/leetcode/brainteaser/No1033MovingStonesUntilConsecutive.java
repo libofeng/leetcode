70e93a60cbd6818d54678f861cb936ff9ce6cd7f
Bofeng Li <bofeng.li@outlook.com>
2019-06-19 21:29:19 +0800

    1033. Moving Stones Until Consecutive

---
+++ src/com/leetcode/brainteaser/No1033MovingStonesUntilConsecutive.java
@@ -0,0 +1,13 @@
+package com.leetcode.brainteaser;
+
+public class No1033MovingStonesUntilConsecutive {
+    public int[] numMovesStones(int a, int b, int c) {
+        int max = Math.max(Math.max(a, b), c);
+        int min = Math.min(Math.min(a, b), c);
+        int mid = a + b + c - max - min;
+
+        int maxMove = max - min - 2;
+        int minMove = maxMove < 2 ? maxMove : ((mid - min <= 2 || max - mid <= 2) ? 1 : 2);
+        return new int[]{minMove, maxMove};
+    }
+}
