cf51fa29b3ddbec05b482a62765f95e2d5f5f298
Bofeng Li <bofeng.li@outlook.com>
2019-06-19 22:38:05 +0800

    292. Nim Game

---
+++ src/com/leetcode/minimax/No292NimGame.java
@@ -0,0 +1,4 @@
+package com.leetcode.minimax;
+
+public class No292NimGame extends com.leetcode.brainteaser.No292NimGame {
+}
---
+++ src/com/leetcode/minimax/No294FlipGameII.java
@@ -0,0 +1,4 @@
+package com.leetcode.minimax;
+
+public class No294FlipGameII extends com.leetcode.string.No294FlipGameII {
+}
