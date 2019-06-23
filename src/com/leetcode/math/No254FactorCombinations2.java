1e3d239c3770a10d6dd4d65e9b57ded56fa904c0
Bofeng Li <bofeng.li@outlook.com>
2019-06-23 17:26:12 +0800

    254. Factor Combinations

--- src/com/leetcode/math/No254FactorCombinations2.java
+++ src/com/leetcode/math/No254FactorCombinations2.java
@@ -3,7 +3,7 @@ package com.leetcode.math;
 import java.util.ArrayList;
 import java.util.List;
 
-public class No254FactorCombinations {
+public class No254FactorCombinations2 {
     public List<List<Integer>> getFactors(int n) {
         final List<List<Integer>> result = new ArrayList<>();
 
@@ -12,15 +12,15 @@ public class No254FactorCombinations {
     }
 
     private void dfs(int n, int start, List<List<Integer>> result, List<Integer> list) {
-        if (n == 1) {
-            if (list.size() > 1) result.add(new ArrayList<>(list));
-            return;
-        }
-
-        for (int i = start; i <= n; i++) {
+        for (int i = start; i * i <= n; i++) {
             if (n % i > 0) continue;
 
             list.add(i);
+
+            list.add(n / i);
+            result.add(new ArrayList<>(list));
+            list.remove(list.size() - 1);
+
             dfs(n / i, i, result, list);
             list.remove(list.size() - 1);
         }
