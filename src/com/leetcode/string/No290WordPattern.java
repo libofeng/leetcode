98c5121c4744c62801beabe1327d8c58baa22315
Bofeng Li <bofeng.li@outlook.com>
2019-06-25 17:12:36 +0800

    290. Word Pattern

--- src/com/leetcode/string/No290WordPattern.java
+++ src/com/leetcode/string/No290WordPattern.java
@@ -7,15 +7,19 @@ public class No290WordPattern {
     public boolean wordPattern(String pattern, String str) {
         final Map<Character, String> ab = new HashMap<>();
         final Map<String, Character> ba = new HashMap<>();
-        final String[] words = str.split(" ");
-        if (pattern.length() != words.length) return false;
-        for (int i = 0; i < pattern.length(); i++) {
-            char p = pattern.charAt(i);
-            String w = words[i];
-            if (ab.containsKey(p)) if (!ab.get(p).equals(w)) return false;
-            if (ba.containsKey(w)) if (ba.get(w) != p) return false;
 
-            ab.put(p, w);
+        String[] words = str.split(" ");
+        if(pattern.length() != words.length) return false;
+
+        for(int i = 0;i<words.length;i++){
+            char a = pattern.charAt(i);
+            String b = words[i];
+
+            if(ab.containsKey(a) && !b.equals(ab.get(a))) return false;
+            if(ba.containsKey(b) && a != ba.get(b)) return false;
+
+            ab.put(a, b);
+            ba.put(b, a);
         }
 
         return true;
