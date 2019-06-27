0a2ecb83dc78b5584e1af00f37d01683f16c8a48
Bofeng Li <bofeng.li@outlook.com>
2019-06-27 14:33:44 +0800

    289. game of life.

--- src/com/leetcode/array/No289GameOfLife.java
+++ src/com/leetcode/array/No289GameOfLife.java
@@ -1,18 +1,17 @@
 package com.leetcode.array;
 
 public class No289GameOfLife {
-    public void gameOfLife(int[][] board) {
-        final int[] dx = new int[]{-1, -1, -1, 0, 1, 1, 1, 0};
-        final int[] dy = new int[]{-1, 0, 1, 1, 1, 0, -1, -1};
-        final int xLen = board.length, yLen = board[0].length;
+    private int[][] dirs = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
-        for (int i = 0; i < xLen; i++) {
-            for (int j = 0; j < yLen; j++) {
+    public void gameOfLife(int[][] board) {
+        final int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 int living = 0;
-                for (int k = 0; k < 8; k++) {
-                    final int x = i + dx[k], y = j + dy[k];
-                    if (x > -1 && y > -1 && x < xLen && y < yLen &&
-                            (board[x][y] == 1 || board[x][y] == 2)) living++;
+                for (int[] dir : dirs) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if (x < 0 || y < 0 || x >= m || y >= n) continue;
+                    if (board[x][y] == 1 || board[x][y] == 2) living++;
                 }
 
                 if (board[i][j] == 1 && (living < 2 || living > 3)) board[i][j] = 2;
@@ -20,10 +19,8 @@ public class No289GameOfLife {
             }
         }
 
-        for (int i = 0; i < xLen; i++) {
-            for (int j = 0; j < yLen; j++) {
-                board[i][j] = board[i][j] % 2;
-            }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) board[i][j] = (board[i][j] & 1);
         }
     }
 }
