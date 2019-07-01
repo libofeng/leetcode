c141f0adc1f593d883a1662b07b8ecd88fefefdf
Bofeng Li <bofeng.li@outlook.com>
2019-07-01 23:08:24 +0800

    365. Water and Jug Problem

---
+++ src/com/leetcode/math/No365WaterAndJugProblem.java
@@ -0,0 +1,20 @@
+package com.leetcode.math;
+
+public class No365WaterAndJugProblem {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (x + y < z) return false;
+        if (x == z || y == z || x + y == z) return true;
+
+        return z % gcd(x, y) == 0;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+
+        return a;
+    }
+}
