f950ce3922e9bfc5447f8407c56a94cc5735cafc
Bofeng Li <bofeng.li@outlook.com>
2019-07-03 21:10:53 +0800

    407. Trapping Rain Water II

--- src/com/leetcode/array/No407TrappingRainWaterII.java
+++ src/com/leetcode/array/No407TrappingRainWaterII.java
@@ -53,4 +53,46 @@ public class No407TrappingRainWaterII {
 
         return R;
     }
+
+    // the same idea as above.
+    public int trapRainWater2(int[][] heightMap) {
+        final int m = heightMap.length, n = m == 0 ? 0 : heightMap[0].length;
+        int total = 0;
+        if (m <= 2 || n <= 2) return total;
+
+        final PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        final boolean[][] visited = new boolean[m][n];
+        final int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int i = 0; i < m; i++) {
+            pq.offer(new int[]{i, 0, heightMap[i][0]});
+            visited[i][0] = true;
+            pq.offer(new int[]{i, n - 1, heightMap[i][n - 1]});
+            visited[i][n - 1] = true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[]{0, i, heightMap[0][i]});
+            visited[0][i] = true;
+            pq.offer(new int[]{m - 1, i, heightMap[m - 1][i]});
+            visited[m - 1][i] = true;
+        }
+
+        int max = 0;
+        while (!pq.isEmpty()) {
+            int i = pq.peek()[0], j = pq.peek()[1], val = pq.poll()[2];
+            max = Math.max(max, val);
+
+            for (int[] dir : dirs) {
+                int x = i + dir[0], y = j + dir[1];
+                if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y]) continue;
+
+                total += Math.max(0, max - heightMap[x][y]);
+                visited[x][y] = true;
+                pq.offer(new int[]{x, y, heightMap[x][y]});
+            }
+        }
+
+        return total;
+    }
 }
