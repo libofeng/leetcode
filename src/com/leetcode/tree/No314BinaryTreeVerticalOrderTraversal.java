8e17a2037ef182886d6daafb818192a8202be2fd
Bofeng Li <bofeng.li@outlook.com>
2019-07-04 16:52:43 +0800

    314. Binary Tree Vertical Order Traversal

--- src/com/leetcode/tree/No314BinaryTreeVerticalOrderTraversal.java
+++ src/com/leetcode/tree/No314BinaryTreeVerticalOrderTraversal.java
@@ -44,4 +44,31 @@ public class No314BinaryTreeVerticalOrderTraversal {
         dfs(root.left, map, col - 1);
         dfs(root.right, map, col + 1);
     }
+
+    //Without using the lambda, this implementation has better performance.
+    public List<List<Integer>> verticalOrder3(TreeNode root) {
+        final List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        final Queue<ColumnNode> q = new LinkedList<>();
+        final Map<Integer, List<Integer>> map = new HashMap<>();
+        int min = 0, max = 0;
+
+        q.offer(new ColumnNode(root, 0));
+        while (!q.isEmpty()) {
+            TreeNode node = q.peek().node;
+            int col = q.poll().col;
+
+            min = Math.min(min, col);
+            max = Math.max(max, col);
+            if (!map.containsKey(col)) map.put(col, new ArrayList<>());
+            map.get(col).add(node.val);
+
+            if (node.left != null) q.offer(new ColumnNode(node.left, col - 1));
+            if (node.right != null) q.offer(new ColumnNode(node.right, col + 1));
+        }
+
+        for (int i = min; i <= max; i++) result.add(map.get(i));
+        return result;
+    }
 }
