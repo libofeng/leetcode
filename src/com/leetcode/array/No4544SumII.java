8db6344e271e5b181bb894875224a964fe5de168
Bofeng Li <bofeng.li@outlook.com>
2019-07-13 10:58:32 +0800

    454. 4Sum II

--- src/com/leetcode/array/No4544SumII.java
+++ src/com/leetcode/array/No4544SumII.java
@@ -20,22 +20,11 @@ public class No4544SumII {
     }
 
     public int fourSumCount2(int[] A, int[] B, int[] C, int[] D) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        for (int a : A) for (int b : B) map.put((a + b), map.getOrDefault((a + b), 0) + 1);
+
         int count = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int nA : A) {
-            for (int nB : B) {
-                int sum = nA + nB;
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
-            }
-        }
-
-        for (int nC : C) {
-            for (int nD : D) {
-                count += map.getOrDefault(-nC - nD, 0);
-            }
-        }
-
+        for (int c : C) for (int d : D) count += map.getOrDefault(-c - d, 0);
         return count;
     }
 }
