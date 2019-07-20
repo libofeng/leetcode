329f066e9c94a54c2fba363137ffaa3647360e0c
Bofeng Li <bofeng.li@outlook.com>
2019-07-21 00:00:48 +0800

    2. Add Two Numbers

--- src/com/leetcode/list/No2AddTwoNumbers.java
+++ src/com/leetcode/list/No2AddTwoNumbers.java
@@ -21,4 +21,20 @@ public class No2AddTwoNumbers {
 
         return dummy.next;
     }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        return add(l1, l2, 0);
+    }
+
+    private ListNode add(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null) return carry > 0 ? new ListNode(carry) : null;
+
+        int a = l1 == null ? 0 : l1.val, b = l2 == null ? 0 : l2.val;
+        int n = a + b + carry;
+        carry = n / 10;
+        final ListNode node = new ListNode(n % 10);
+        node.next = add(l1 == null ? null : l1.next, l2 == null ? null : l2.next, carry);
+
+        return node;
+    }
 }
