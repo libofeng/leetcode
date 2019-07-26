6002e9f7e27f748ece70ad404911e2ae0de03c84
Bofeng Li <bofeng.li@outlook.com>
2019-07-26 16:33:28 +0800

    73. Set Matrix Zeroes using stream API.

--- src/com/leetcode/array/No73SetMatrixZeroes.java
+++ src/com/leetcode/array/No73SetMatrixZeroes.java
@@ -1,5 +1,7 @@
 package com.leetcode.array;
 
+import java.util.Arrays;
+
 public class No73SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
         boolean rowZero = false, columnZero = false;
@@ -25,5 +27,22 @@ public class No73SetMatrixZeroes {
 
         if (rowZero) for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
         if (columnZero) for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+    }
+
+    public void setZeroes2(int[][] matrix) {
+        final int m = matrix.length, n = matrix[0].length;
+        boolean rowZero = Arrays.stream(matrix[0]).anyMatch(k -> k == 0);
+        boolean colZero = Arrays.stream(matrix).anyMatch(k -> k[0] == 0);
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) if (matrix[i][j] == 0) matrix[0][j] = matrix[i][0] = 0;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+        }
+
+        if (rowZero) Arrays.fill(matrix[0], 0);
+        if (colZero) for (int i = 0; i < m; i++) matrix[i][0] = 0;
     }
 }
