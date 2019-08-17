6cc3875cf7b43d47c46be573ba1ae1c48ba9298c
Bofeng Li <bofeng.li@outlook.com>
2019-08-17 23:35:33 +0800

    329. Longest Increasing Path in a Matrix
    Update topological sort solution.

--- src/com/leetcode/array/No329LongestIncreasingPathInAMatrixTopLogic.java
+++ src/com/leetcode/array/No329LongestIncreasingPathInAMatrixTopLogic.java
@@ -3,62 +3,55 @@ package com.leetcode.array;
 import java.util.*;
 
 public class No329LongestIncreasingPathInAMatrixTopLogic {
-    public int longestIncreasingPath(int[][] matrix) {
-        if (matrix.length < 1) return 0;
+    private final int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        Map<Point, Set<Point>> graph = new HashMap<>();
-        Map<Point, Integer> indegree = new HashMap<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix.length == 0) return 0;
+        final int m = matrix.length, n = matrix[0].length;
+        final Map<Point, List<Point>> graph = new HashMap<>();
+        final Map<Point, Integer> inDegree = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 Point p = new Point(i, j);
-                if (i > 0 && matrix[i - 1][j] > matrix[i][j]) {
-                    Point next = new Point(i - 1, j);
-                    graph.computeIfAbsent(p, k -> new HashSet<>()).add(next);
-                    indegree.put(next, indegree.getOrDefault(next, 0) + 1);
-                }
-                if (j > 0 && matrix[i][j - 1] > matrix[i][j]) {
-                    Point next = new Point(i, j - 1);
-                    graph.computeIfAbsent(p, k -> new HashSet<>()).add(next);
-                    indegree.put(next, indegree.getOrDefault(next, 0) + 1);
-                }
-                if (i < matrix.length - 1 && matrix[i + 1][j] > matrix[i][j]) {
-                    Point next = new Point(i + 1, j);
-                    graph.computeIfAbsent(p, k -> new HashSet<>()).add(next);
-                    indegree.put(next, indegree.getOrDefault(next, 0) + 1);
-                }
-                if (j < matrix[i].length - 1 && matrix[i][j + 1] > matrix[i][j]) {
-                    Point next = new Point(i, j + 1);
-                    graph.computeIfAbsent(p, k -> new HashSet<>()).add(next);
-                    indegree.put(next, indegree.getOrDefault(next, 0) + 1);
+                graph.putIfAbsent(p, new ArrayList<>());
+                inDegree.putIfAbsent(p, 0);
+
+                for (int[] dir : dirs) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if (x < 0 || y < 0 || x >= m || y >= n || matrix[x][y] <= matrix[i][j]) continue;
+
+                    Point c = new Point(x, y);
+                    graph.get(p).add(c);
+                    inDegree.put(c, inDegree.getOrDefault(c, 0) + 1);
                 }
             }
         }
-        Queue<Object[]> q = new LinkedList<>();
-        int max = 1;
-        for (Point p : graph.keySet()) {
-            if (indegree.getOrDefault(p, 0) == 0) {
-                q.add(new Object[]{p, 0});
-            }
+
+        final Queue<Object[]> q = new LinkedList<>();
+        for (Map.Entry<Point, Integer> e : inDegree.entrySet()) {
+            if (e.getValue() == 0) q.offer(new Object[]{e.getKey(), 0});
         }
+
+        int maxLen = 0;
         while (!q.isEmpty()) {
-            Object[] obj = q.remove();
-            Point p = (Point) obj[0];
-            int pathLen = (int) obj[1];
-            max = Math.max(max, ++pathLen);
-            for (Point next : graph.getOrDefault(p, Collections.emptySet())) {
-                indegree.put(next, indegree.get(next) - 1);
-                if (indegree.get(next) == 0) {
-                    q.add(new Object[]{next, pathLen});
-                }
+            Point p = (Point) q.peek()[0];
+            int len = (Integer) q.poll()[1];
+            maxLen = Math.max(maxLen, ++len);
+
+            for (Point next : graph.get(p)) {
+                inDegree.put(next, inDegree.get(next) - 1);
+                if (inDegree.get(next) == 0) q.offer(new Object[]{next, len});
             }
         }
-        return max;
+
+        return maxLen;
     }
 
-    private static class Point {
+    class Point {
         int x, y;
 
-        public Point(int x, int y) {
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
@@ -70,8 +63,8 @@ public class No329LongestIncreasingPathInAMatrixTopLogic {
 
         @Override
         public boolean equals(Object o) {
-            Point p = (Point) o;
-            return p != null && p.x == x && p.y == y;
+            Point that = (Point) o;
+            return that != null && this.x == that.x && this.y == that.y;
         }
     }
 }
