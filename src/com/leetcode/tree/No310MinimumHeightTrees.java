b43168ece91b40c5ca94da4cb9b8c4a7b3b582b5
Bofeng Li <bofeng.li@outlook.com>
2019-08-22 22:31:49 +0800

    310. Minimum Height Trees

--- src/com/leetcode/tree/No310MinimumHeightTrees.java
+++ src/com/leetcode/tree/No310MinimumHeightTrees.java
@@ -10,26 +10,29 @@ public class No310MinimumHeightTrees {
 
         final List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
         for (int[] e : edges) {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
 
-        List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; i++) if (graph.get(i).size() == 1) leaves.add(i);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) if (graph.get(i).size() == 1) list.add(i);
 
         while (n > 2) {
-            n -= leaves.size();
+            n -= list.size();
+
             List<Integer> next = new ArrayList<>();
-            for (Integer i : leaves) {
-                int j = graph.get(i).iterator().next();
-                graph.get(j).remove(i);
-                if (graph.get(j).size() == 1) next.add(j);
+            for (Integer node : list) {
+                int adj = graph.get(node).remove(0);
+                graph.get(adj).remove(node);
+
+                if (graph.get(adj).size() == 1) next.add(adj);
             }
 
-            leaves = next;
+            list = next;
         }
 
-        return leaves;
+        return list;
     }
 }
