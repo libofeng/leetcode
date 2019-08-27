20726f2d1cd4fb802c86fb9b3d13cdd3239cb406
Bofeng Li <bofeng.li@outlook.com>
2019-08-27 22:46:50 +0800

    1168. Optimize Water Distribution in a Village

--- src/com/leetcode/contest/biweekly/contest7/No1168OptimizeWaterDistributionInAVillage.java
+++ src/com/leetcode/contest/biweekly/contest7/No1168OptimizeWaterDistributionInAVillage.java
@@ -6,29 +6,24 @@ public class No1168OptimizeWaterDistributionInAVillage {
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
         final List<List<int[]>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-
         for (int[] e : pipes) {
             graph.get(e[0] - 1).add(new int[]{e[1] - 1, e[2]});
             graph.get(e[1] - 1).add(new int[]{e[0] - 1, e[2]});
         }
 
+        final Set<Integer> visited = new HashSet<>();
         final Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         for (int i = 0; i < n; i++) pq.offer(new int[]{i, wells[i]});
 
-        final Set<Integer> visited = new HashSet<>();
-
-        int cost = 0;
+        int costs = 0;
         while (!pq.isEmpty()) {
-            int w = pq.peek()[0], c = pq.poll()[1];
-            if (!visited.add(w)) continue;
+            int h = pq.peek()[0], cost = pq.poll()[1];
+            if (!visited.add(h)) continue;
 
-            cost += c;
-            for (int[] next : graph.get(w)) {
-                int nextCost = Math.min(next[1], wells[next[0]]);
-                pq.offer(new int[]{next[0], nextCost});
-            }
+            costs += cost;
+            for (int[] next : graph.get(h)) pq.offer(next);
         }
 
-        return cost;
+        return visited.size() == n ? costs : -1;
     }
 }
